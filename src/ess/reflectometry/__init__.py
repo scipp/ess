7252468604370a3bb1c4d2781393e367b0ceb767
Neil Vaytet <neil.vaytet@esss.se>
2022-09-30 14:11:27 +0200

    flake8

--- src/ess/reflectometry/__init__.py
+++ src/ess/reflectometry/__init__.py
@@ -1,6 +1,8 @@
 # SPDX-License-Identifier: BSD-3-Clause
 # Copyright (c) 2022 Scipp contributors (https://github.com/scipp)
-# flake8 : noqa: F401
+
+# flake8: noqa: F401
+
 from . import corrections
 from . import conversions
 from . import io
--- src/ess/sans/__init__.py
+++ src/ess/sans/__init__.py
@@ -1,6 +1,8 @@
 # SPDX-License-Identifier: BSD-3-Clause
 # Copyright (c) 2022 Scipp contributors (https://github.com/scipp)
 
+# flake8: noqa: F401
+
 from . import common
 from . import conversions
 from . import normalization
