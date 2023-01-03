6be3ee27541fbf41287aefa156b0509f00594afd
Neil Vaytet <neil.vaytet@esss.se>
2023-01-03 15:25:56 +0100

    one more int64?

--- tests/diffraction/filtering_test.py
+++ tests/diffraction/filtering_test.py
@@ -33,7 +33,9 @@ def make_data_with_pulse_time(rng, n_event):
                           })
     return sc.binning.make_binned(
         events,
-        edges=[sc.array(dims=['tof'], values=[10, 500, 1000], unit='us')],
+        edges=[
+            sc.array(dims=['tof'], values=[10, 500, 1000], unit='us', dtype='int64')
+        ],
         groups=[sc.arange('spectrum', 0, 10, unit=None, dtype='int64')])
 
 
