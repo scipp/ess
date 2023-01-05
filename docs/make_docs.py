4e6fd79ef83bed3b42cd96088555ddfd74529827
Neil Vaytet <neil.vaytet@esss.se>
2023-01-05 16:20:40 +0100

    simplify paths

--- docs/make_docs.py
+++ docs/make_docs.py
@@ -12,35 +12,24 @@ parser.add_argument('--build-dir', default='build')
 parser.add_argument('--work-dir', default='.doctrees')
 parser.add_argument('--builder', default='html')
 
-
-def get_abs_path(path, root):
-    if os.path.isabs(path):
-        return path
-    else:
-        return os.path.join(root, path)
-
-
 if __name__ == '__main__':
     args = parser.parse_args()
 
-    cwd = os.getcwd()
     docs_dir = pathlib.Path(__file__).parent.absolute()
-    work_dir = os.path.join(cwd, get_abs_path(path=args.work_dir, root=docs_dir))
-    build_dir = os.path.join(cwd, get_abs_path(path=args.build_dir, root=docs_dir))
-    if not os.path.exists(build_dir):
-        os.makedirs(build_dir)
 
     # Build the docs with sphinx-build
-    subprocess.check_call(
-        ['sphinx-build', '-v', '-b', args.builder, '-d', work_dir, docs_dir, build_dir],
-        stderr=subprocess.STDOUT,
-        shell=sys.platform == "win32")
+    subprocess.check_call([
+        'sphinx-build', '-v', '-b', args.builder, '-d', args.work_dir, docs_dir,
+        args.build_dir
+    ],
+                          stderr=subprocess.STDOUT,
+                          shell=sys.platform == "win32")
 
     # Remove Jupyter notebooks used for documentation build,
     # they are not accessible and create size bloat.
     # However, keep the ones in the `_sources` folder,
     # as the download buttons links to them.
-    sources_dir = os.path.join(build_dir, '_sources')
-    for path in pathlib.Path(build_dir).rglob('*.ipynb'):
+    sources_dir = os.path.join(args.build_dir, '_sources')
+    for path in pathlib.Path(args.build_dir).rglob('*.ipynb'):
         if not str(path).startswith(sources_dir):
             os.remove(path)
