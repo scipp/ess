2ed489a2e77c8fed6c0d6e1d0ad0faf39a777eb8
Neil Vaytet <neil.vaytet@esss.se>
2023-01-09 16:39:07 +0100

    update copyright year

--- src/ess/nexus/__init__.py
+++ src/ess/nexus/__init__.py
@@ -1,5 +1,5 @@
 # SPDX-License-Identifier: BSD-3-Clause
-# Copyright (c) 2022 Scipp contributors (https://github.com/scipp)
+# Copyright (c) 2023 Scipp contributors (https://github.com/scipp)
 # Author: Simon Heybrock
 """
 This package contains components (functions and classes) for custom loaders for NeXus
