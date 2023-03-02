90719894dd1304bfe92b3413c740c0b87565b461
Neil Vaytet <neil.vaytet@esss.se>
2023-03-02 09:33:17 +0100

    update hdf5 file hashes

--- src/ess/loki/data.py
+++ src/ess/loki/data.py
@@ -16,13 +16,13 @@ def _make_pooch():
             'DIRECT_SANS2D_REAR_34327_4m_8mm_16Feb16.dat':
             'md5:d64495831325a63e1b961776a8544599',
             'DIRECT_SANS2D_REAR_34327_4m_8mm_16Feb16.hdf5':
-            'md5:45b6f5dafbfc5d994a048791653fab65',
+            'md5:43f4188301d709aa49df0631d03a67cb',
             'SANS2D00063091.nxs': 'md5:05929753ea06eca5fe4be164cb06b4d6',
-            'SANS2D00063091.hdf5': 'md5:851e8c89027599c312387b3220404295',
+            'SANS2D00063091.hdf5': 'md5:c212f1c8c68db69eae88eca90a19e7e6',
             'SANS2D00063114.nxs': 'md5:b3a3f7527ef871d728942cac3da52289',
-            'SANS2D00063114.hdf5': 'md5:de07bb6e0947cce920499d61b5629b0f',
+            'SANS2D00063114.hdf5': 'md5:806a5780ff02676afcea1c3d8777ee21',
             'SANS2D00063159.nxs': 'md5:c0a0f376964bd9e8c6364552bc1f94e1',
-            'SANS2D00063159.hdf5': 'md5:c0f1d38bd94d799ed54ae14ffb2520ad',
+            'SANS2D00063159.hdf5': 'md5:7be098bcc1f4ca73394584076a99146d',
             'SANS2D_Definition_Tubes.xml': 'md5:ea988c64119bf9eaaa004e5bc41b8c40'
         })
 
