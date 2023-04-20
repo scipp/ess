156f77a4b36d86916fc83b6d14da24e72d2d7158
Andrew McCluskey <andrew.mccluskey@ess.eu>
2023-04-20 16:23:47 +0200

    Working offspec data reduction

--- src/ess/reflectometry/io.py
+++ src/ess/reflectometry/io.py
@@ -18,16 +18,16 @@ def save_ort(data_array: sc.DataArray, filename: str, dimension: str = None):
     dimension:
         String for dimension to perform mean over, defaults to 'detector_id'.
     """
-    if dimension is None:
-        dimension = 'detector_id'
     from orsopy import fileio
     if filename[:-4] == '.ort':
         raise ValueError("The expected output file ending is .ort.")
-    q = data_array.mean(dimension).coords['Q']
-    if data_array.mean(dimension).coords.is_edges('Q'):
+    if dimension is not None:
+        data_array = data_array.mean(dimension)
+    q = data_array.coords['Q']
+    if data_array.coords.is_edges('Q'):
         q = sc.midpoints(q)
-    R = data_array.mean(dimension).data
-    sR = sc.stddevs(data_array.mean(dimension).data)
+    R = data_array.data
+    sR = sc.stddevs(data_array.data)
     sq = data_array.coords['sigma_Q']
     dataset = fileio.orso.OrsoDataset(
         data_array.attrs['orso'].value,
