c0a1716865994a9bd7f0f21d162bbce0bf675728
Neil Vaytet <neil.vaytet@esss.se>
2023-06-28 10:16:05 +0200

    generalize instrument view

--- src/ess/dream/instrument_view.py
+++ src/ess/dream/instrument_view.py
@@ -9,10 +9,14 @@ import scipp as sc
 
 def instrument_view(
     data: Union[sc.DataArray, sc.DataGroup],
+    x: Optional[str] = None,
+    y: Optional[str] = None,
+    z: Optional[str] = None,
+    pos: Optional[str] = None,
     dim: Optional[str] = None,
     bins: Union[sc.Variable, int] = 50,
     pixel_size: Union[sc.Variable, float] = 10,
-    **kwargs
+    **kwargs,
 ):
     """
     Three-dimensional visualization of the DREAM instrument pixels.
@@ -24,6 +28,17 @@ def instrument_view(
     ----------
     data:
         Data to visualize.
+    x:
+        The name of the coordinate that is to be used for the X positions.
+        Default is 'x'.
+    y:
+        The name of the coordinate that is to be used for the Y positions.
+        Default is 'y'.
+    z:
+        The name of the coordinate that is to be used for the Z positions.
+        Default is 'z'.
+    pos:
+        The name of the vector coordinate that is to be used for the positions.
     dim:
         Dimension to use for the slider.
     bins:
@@ -31,6 +46,9 @@ def instrument_view(
     pixel_size:
         Size of the pixels. If a float is provided, it will assume the same unit as the
         pixel coordinates.
+    **kwargs:
+        Additional arguments to pass to the plopp figure
+        (see https://scipp.github.io/plopp/about/generated/plopp.scatter3d.html).
     """
     from plopp.graphics import figure3d
     import plopp.widgets as pw
@@ -39,6 +57,26 @@ def instrument_view(
         data = sc.concat([da.flatten(to='pixel') for da in data.values()], dim='pixel')
     else:
         data = data.flatten(to='pixel')
+
+    if pos is not None:
+        if any((x, y, z)):
+            raise ValueError(
+                f'If pos ({pos}) is defined, all of '
+                f'x ({x}), y ({y}), and z ({z}) must be None.'
+            )
+        coords = {
+            (x := f'{pos}.x'): data.coords[pos].fields.x,
+            (y := f'{pos}.y'): data.coords[pos].fields.y,
+            (z := f'{pos}.z'): data.coords[pos].fields.z,
+        }
+    else:
+        x = x if x is not None else 'x'
+        y = y if y is not None else 'y'
+        z = z if z is not None else 'z'
+        coords = {k: data.coords[k] for k in (x, y, z)}
+
+    # No need to make a copy here because one was made higher up with `flatten`.
+    data.coords.update(coords)
 
     if dim is not None:
         histogrammed = data.hist({dim: bins}) if data.bins is not None else data
@@ -49,7 +87,7 @@ def instrument_view(
     else:
         nodes = [pp.Node(data.hist() if data.bins is not None else data)]
 
-    fig = figure3d(*nodes, x='x', y='y', z='z', pixel_size=pixel_size, **kwargs)
+    fig = figure3d(*nodes, x=x, y=y, z=z, pixel_size=pixel_size, **kwargs)
     tri_cutter = pw.TriCutTool(fig)
     fig.toolbar['cut3d'] = pw.ToggleTool(
         callback=tri_cutter.toggle_visibility,
