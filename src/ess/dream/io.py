f89073a5ad88194f2d62af5e0d981510be844260
Simon Heybrock <simon.heybrock@ess.eu>
2023-08-28 13:49:48 +0200

    Also reorder det nums and offsets

--- src/ess/dream/io.py
+++ src/ess/dream/io.py
@@ -5,23 +5,38 @@ import scipp as sc
 import scippnexus as snx
 
 
-def load_nexus(path: Union[str, os.PathLike]):
+def load_nexus(path: Union[str, os.PathLike], load_pixel_shape: bool = False):
+    """Load dream_mantle_test.nxs file."""
     definitions = snx.base_definitions()
-    definitions["NXdetector"] = FilteredDetector
+    if not load_pixel_shape:
+        definitions["NXdetector"] = FilteredDetector
 
     with snx.File(path, definitions=definitions) as f:
         dg = f['entry'][()]
     if 'mantel' in dg['instrument']:
         dg['instrument']['mantle'] = dg['instrument'].pop('mantel')
-    # This file is (counter:2, hemisphere:2, module:5, segment:6, wire:32, strip:128)
-    sizes = {
-        'counter': 2,
-        'hemisphere': 2,
-        'module': 5,
-        'segment': 6,
-        'wire': 32,
-        'strip': 128,
-    }
+    # The file has (counter:2, hemisphere:2, module:5, segment:6, wire:32, strip:128)
+    mantle = dg['instrument']['mantle']
+    comps = mantle.data.bins.constituents
+    for key in ['begin', 'end']:
+        comps[key] = _reorder_voxels(comps[key])
+    for key in [
+        'detector_number',
+        'x_pixel_offset',
+        'y_pixel_offset',
+        'z_pixel_offset',
+    ]:
+        mantle.coords[key] = _reorder_voxels(mantle.coords[key])
+
+    mantle.data = sc.bins(**comps)
+    # For convenience, combine (module,segment,counter) into "subsegment"
+    dg['instrument']['mantle'] = mantle.fold(
+        'detector_number', sizes={'subsegment': 5 * 6 * 2, 'wire': 32, 'strip': 256}
+    )
+    return dg
+
+
+def _reorder_voxels(var: sc.Variable) -> sc.Variable:
     # Scipp only supports init from max 4-D NumPy arrays, workaround:
     flatsizes = {
         'counter': 2,
@@ -29,29 +44,20 @@ def load_nexus(path: Union[str, os.PathLike]):
         'dummy': 5 * 6 * 32,
         'strip': 128,
     }
-    mantle = dg['instrument']['mantle'].data.fold('detector_number', sizes=flatsizes)
-    comps = mantle.bins.constituents
-    for key in ['begin', 'end']:
-        lower = comps[key]['hemisphere', 0]
-        upper = comps[key]['hemisphere', 1]
-        # Reverse lower hemisphere strip
-        lower = sc.array(dims=lower.dims, values=lower.values[..., ::-1], unit=None)
-        # Concat hemispheres into common strip index
-        # Move 'counter' inside 'segment' dim to match physical ordering
-        dims = ('module', 'segment', 'counter', 'wire', 'strip')
-        comps[key] = (
-            sc.concat([lower, upper], 'strip')
-            .fold('dummy', sizes={'module': 5, 'segment': 6, 'wire': 32})
-            .transpose(dims)
-            .copy()
-        )
-
-    dg['instrument']['mantle'].data = sc.bins(**comps).flatten(to='detector_number')
-    # For convenience, combine (module,segment,counter) into "subsegment"
-    dg['instrument']['mantle'] = dg['instrument']['mantle'].fold(
-        'detector_number', sizes={'subsegment': 5 * 6 * 2, 'wire': 32, 'strip': 256}
+    var = var.fold('detector_number', sizes=flatsizes)
+    lower = var['hemisphere', 0]
+    upper = var['hemisphere', 1]
+    # Reverse lower hemisphere strip
+    lower = sc.array(dims=lower.dims, values=lower.values[..., ::-1], unit=lower.unit)
+    # Concat hemispheres into common strip index
+    # Move 'counter' inside 'segment' dim to match physical ordering
+    dims = ('module', 'segment', 'counter', 'wire', 'strip')
+    return (
+        sc.concat([lower, upper], 'strip')
+        .fold('dummy', sizes={'module': 5, 'segment': 6, 'wire': 32})
+        .transpose(dims)
+        .flatten(to='detector_number')
     )
-    return dg
 
 
 def _skip(name: str, obj: Union[snx.Field, snx.Group]) -> bool:
