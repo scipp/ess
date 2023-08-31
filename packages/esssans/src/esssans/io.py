478b8f6f2a4ebe6d8eed0a59fed6acce76121847
Simon Heybrock <simon.heybrock@ess.eu>
2023-08-31 08:57:45 +0200

    Refactor masking

--- packages/esssans/src/esssans/io.py
+++ packages/esssans/src/esssans/io.py
@@ -1,7 +1,18 @@
 # SPDX-License-Identifier: BSD-3-Clause
 # Copyright (c) 2023 Scipp contributors (https://github.com/scipp)
 import scipp as sc
-from .types import Filename, RunType, RawData, MonitorType, RawMonitor, NeXusMonitorName
+from .types import (
+    Filename,
+    RunType,
+    RawData,
+    MonitorType,
+    RawMonitor,
+    NeXusMonitorName,
+    DetectorEdgeMask,
+    SampleHolderMask,
+    SampleRun,
+    MaskedData,
+)
 
 
 def load(filename: Filename[RunType]) -> RawData[RunType]:
@@ -16,4 +27,32 @@ def get_monitor(
     return IncidentMonitor[RunType](da.attrs['monitor2'].value)
 
 
-providers = [load, get_monitor]
+def detector_edge_mask(sample: RawData[SampleRun]) -> DetectorEdgeMask:
+    mask_edges = (
+        sc.abs(sample.coords['position'].fields.x) > sc.scalar(0.48, unit='m')
+    ) | (sc.abs(sample.coords['position'].fields.y) > sc.scalar(0.45, unit='m'))
+    return DetectorEdgeMask(mask_edges)
+
+
+def sample_holder_mask(sample: RawData[SampleRun]) -> SampleHolderMask:
+    summed = sample.sum('tof')
+    holder_mask = (
+        (summed.data < sc.scalar(100, unit='counts'))
+        & (sample.coords['position'].fields.x > sc.scalar(0, unit='m'))
+        & (sample.coords['position'].fields.x < sc.scalar(0.42, unit='m'))
+        & (sample.coords['position'].fields.y < sc.scalar(0.05, unit='m'))
+        & (sample.coords['position'].fields.y > sc.scalar(-0.15, unit='m'))
+    )
+    return SampleHolderMask(holder_mask)
+
+
+def mask_detectors(
+    da: RawData[RunType], edge_mask: DetectorEdgeMask, holder_mask: SampleHolderMask
+) -> MaskedData[RunType]:
+    da = da.copy(deep=False)
+    da.masks['edges'] = edge_mask
+    da.masks['holder_mask'] = holder_mask
+    return MaskedData[RunType](da)
+
+
+providers = [load, get_monitor, detector_edge_mask, sample_holder_mask, mask_detectors]
--- packages/esssans/src/esssans/types.py
+++ packages/esssans/src/esssans/types.py
@@ -1,7 +1,7 @@
 # SPDX-License-Identifier: BSD-3-Clause
 # Copyright (c) 2023 Scipp contributors (https://github.com/scipp)
 from dataclasses import dataclass
-from typing import NewType, TypeVar, Generic
+from typing import NewType, TypeVar, Generic, Tuple
 import scipp as sc
 import sciline
 
@@ -9,6 +9,9 @@ WavelengthBins = NewType('WavelengthBins', sc.Variable)
 QBins = NewType('QBins', sc.Variable)
 NonBackgroundWavelengthRange = NewType('NonBackgroundWavelengthRange', sc.Variable)
 DirectBeamFilename = NewType('DirectBeamFilename', str)
+BeamCenter = NewType('BeamCenter', Tuple[sc.Variable, sc.Variable])
+DetectorEdgeMask = NewType('DetectorEdgeMask', sc.Variable)
+SampleHolderMask = NewType('SampleHolderMask', sc.Variable)
 
 SampleRun = NewType('SampleRun', int)
 DirectRun = NewType('DirectRun', int)
@@ -29,6 +32,10 @@ class Filename(sciline.Scope[RunType, str], str):
 
 
 class RawData(sciline.Scope[RunType, sc.DataArray], sc.DataArray):
+    ...
+
+
+class MaskedData(sciline.Scope[RunType, sc.DataArray], sc.DataArray):
     ...
 
 
