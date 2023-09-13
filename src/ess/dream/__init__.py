86d23aa76112aa6c8cbe40b4e2029a75a355f4aa
Neil Vaytet <neil.vaytet@esss.se>
2023-09-13 17:08:19 +0200

    flake8

--- src/ess/dream/__init__.py
+++ src/ess/dream/__init__.py
@@ -1,5 +1,6 @@
 # SPDX-License-Identifier: BSD-3-Clause
 # Copyright (c) 2023 Scipp contributors (https://github.com/scipp)
+# flake8: noqa: F401
 
 from . import data
 from .instrument_view import instrument_view
--- src/ess/dream/data.py
+++ src/ess/dream/data.py
@@ -6,7 +6,7 @@ from ..data import Registry
 _registry = Registry(
     instrument='dream',
     files={
-        'data_dream_HF_mil_closed_alldets_1e9.csv.zip': 'md5:2997f33d3ddc792083bfab661cf1d93a'
+        'data_dream_HF_mil_closed_alldets_1e9.csv.zip': 'md5:2997f33d3ddc792083bfab661cf1d93a'  # noqa: E501
     },
     version='1',
 )
