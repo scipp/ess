a420ed1f61554dadad15dc06b1114ec4e763445a
Jan-Lukas Wynen <jan-lukas.wynen@ess.eu>
2023-09-26 11:12:11 +0200

    Use scipp-hdf5 files instead of ISIS NeXus

--- src/ess/external/offspec/data.py
+++ src/ess/external/offspec/data.py
@@ -13,7 +13,9 @@ def _make_pooch():
         version=_version,
         registry={
             "direct_beam.nxs": "md5:e929d3419b13c3ffa4a5545ec54f9044",
+            "direct_beam.h5": "md5:1c4e56afbd35edd96c7607e357981ccf",
             "sample.nxs": "md5:f18a8122706201df8150e7556ae6eb59",
+            "sample.h5": "md5:02b8703230b6b1e6282c0d39eb94523c",
             "reduced_mantid.xye": "md5:1f372f51d2cefb8dee302cf0093b684f",
         },
     )
@@ -33,11 +35,13 @@ def get_path(name: str) -> str:
 
 
 def direct_beam() -> str:
-    return get_path('direct_beam.nxs')
+    """Scipp HDF5 file for a direct beam run."""
+    return get_path('direct_beam.h5')
 
 
 def sample() -> str:
-    return get_path('sample.nxs')
+    """Scipp HDF5 file for a sample measurement."""
+    return get_path('sample.h5')
 
 
 def reduced_mantid() -> str:
