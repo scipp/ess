ba246367a1a84e4f2ee1c5ceffedb04206223404
Simon Heybrock <simon.heybrock@ess.eu>
2023-12-04 12:27:47 +0100

    Simpler and more realistic logic for run sections and spin channels

--- packages/esspolarization/src/ess/polarization/base.py
+++ packages/esspolarization/src/ess/polarization/base.py
@@ -9,8 +9,6 @@ Up = NewType('Up', int)
 Down = NewType('Down', int)
 Unpolarized = NewType('Unpolarized', int)
 Spin = TypeVar('Spin', Up, Down, Unpolarized)
-PolarizerSpin = TypeVar('PolarizerSpin', Up, Down)
-AnalyzerSpin = TypeVar('AnalyzerSpin', Up, Down)
 
 Analyzer = NewType('Analyzer', str)
 Polarizer = NewType('Polarizer', str)
@@ -40,15 +38,6 @@ class He3Opacity(sl.Scope[Cell, sc.DataArray], sc.DataArray):
     """Wavelength-dependent opacity for a given cell."""
 
 
-class DirectBeamTimeIntervals(sl.ScopeTwoParams[Cell, Spin, sc.Variable], sc.Variable):
-    """
-    Wall-clock time intervals for a given cell.
-
-    This defines the time intervals that correspond the direct beam measurements. This
-    is used to extract the direct beam data from the total event data.
-    """
-
-
 class He3DirectBeam(sl.ScopeTwoParams[Cell, Spin, sc.DataArray], sc.DataArray):
     """
     Direct beam data for a given cell and spin state.
@@ -66,37 +55,71 @@ class He3InitialAtomicPolarization(sl.Scope[Cell, sc.DataArray], sc.DataArray):
     """
 
 
-class SampleData(
-    sl.ScopeTwoParams[PolarizerSpin, AnalyzerSpin, sc.DataArray], sc.DataArray
-):
+SampleData = NewType('SampleData', sc.DataArray)
+"""
+Uncorrected sample data.
+
+TODO How exactly is this defined? Which normal (non-polarization) corrections
+have been applied already?
+"""
+
+
+PolarizationCorrectedSampleData = NewType(
+    'PolarizationCorrectedSampleData', sc.DataArray
+)
+"""Polarization-corrected sample data."""
+
+
+class CellSpin(sl.Scope[Cell, sc.DataArray], sc.DataArray):
+    """Spin state of a given cell, as a time series."""
+
+
+RunSection = NewType('RunSection', sc.DataArray)
+"""
+Run type (sample/direct/neither/...) as a time series.
+
+This needs to be derived from some time-series logs in the NeXus file, e.g.,
+whether the sample is in the beam or not.
+"""
+
+
+SpinChannel = NewType('SpinChannel', sc.DataArray)
+"""Time series of the combined spin channel (++, +-, -+, --)."""
+
+RawDataByRunSection = NewType('RawDataByRunSection', sc.DataArray)
+"""Raw event data with events labeled (or grouped) by run section (sample/direct)."""
+
+DirectBeamData = NewType('DirectBeamData', sc.DataArray)
+"""
+Raw direct beam event data with events labeled (or grouped) by cell and spin state.
+"""
+
+
+def dummy_cell_spin() -> CellSpin[Cell]:
     """
-    Uncorrected sample data.
+    Return a dummy cell spin.
 
-    TODO How exactly is this defined? Which normal (non-polarization) corrections
-    have been applied already?
+    This needs to be derived from some time-series log in the NeXus file, relating to
+    the switching of the He3 cell.
     """
+    pass
 
 
-class PolarizationCorrectedSampleData(
-    sl.ScopeTwoParams[PolarizerSpin, AnalyzerSpin, sc.DataArray], sc.DataArray
-):
-    """Polarization-corrected sample data."""
-
-
-class SampleTimeIntervals(
-    sl.ScopeTwoParams[PolarizerSpin, AnalyzerSpin, sc.Variable], sc.Variable
-):
+def spin_channel(
+    polarizer_spin: CellSpin[Polarizer], analyzer_spin: CellSpin[Analyzer]
+) -> SpinChannel:
     """
-    Wall-clock time intervals for a given sample.
+    Returns a time series of the combined spin channel (++, +-, -+, --).
 
-    This defines the time intervals that correspond the sample measurements. This is
-    used to extract the sample data from the total event data.
+    This will be used to split the raw detector data into the four spin channels.
     """
+    # TODO In practice, are switching times instant? Do we need to drop events that
+    # occur during switching? How is this marked in the meta data?
+    return SpinChannel()
 
 
 def he3_direct_beam(
-    event_data: RawEventData,
-    time_intervals: DirectBeamTimeIntervals[Cell, Spin],
+    event_data: DirectBeamData,
     wavelength: WavelengthBins,
 ) -> He3DirectBeam[Cell, Spin]:
     """
@@ -145,24 +168,48 @@ def he3_transmission(
     return He3Transmission[Cell](1)
 
 
-def sample_data(
-    event_data: RawEventData,
-    time_intervals: SampleTimeIntervals[PolarizerSpin, AnalyzerSpin],
-) -> SampleData[PolarizerSpin, AnalyzerSpin]:
+def raw_data_by_run_section(
+    raw: RawEventData, run_section: RunSection
+) -> RawDataByRunSection:
     """
-    Wavelength-dependent sample data for a given spin state.
+    Split raw event data into sample data and direct beam data.
+
+    This assigns each raw event to either the sample run or the direct beam run,
+    based in the time-dependent run type.
+
+    The reason for splitting the data here before splitting into spin channels and the
+    various direct runs is to avoid performance issues from multiple passes over the
+    full data set.
+    """
+    return RawDataByRunSection()
+
+
+def sample_data_by_spin_channel(
+    event_data: RawDataByRunSection,
+    spin_channel: SpinChannel,
+) -> SampleData:
+    """
+    Wavelength-dependent sample data for all spin channels.
+
+    This labels event with their spin channel and maybe groups by channel, so we have
+    an output dimension of length 4 (++, +-, -+, --).
     """
     pass
 
 
+def direct_beam_data_by_cell_and_polarization(
+    event_data: RawDataByRunSection,
+    polarizer_spin: CellSpin[Polarizer],
+    analyzer_spin: CellSpin[Analyzer],
+) -> DirectBeamData:
+    """ """
+
+
 def polarization_corrected_sample_data(
-    sample_data_up_up: SampleData[Up, Up],
-    sample_data_up_down: SampleData[Up, Down],
-    sample_data_down_up: SampleData[Down, Up],
-    sample_data_down_down: SampleData[Down, Down],
+    sample_data: SampleData,
     transmission_polarizer: He3Transmission[Polarizer],
     transmission_analyzer: He3Transmission[Analyzer],
-) -> PolarizationCorrectedSampleData[PolarizerSpin, AnalyzerSpin]:
+) -> PolarizationCorrectedSampleData:
     """
     Apply polarization correction for the case of He3 polarizers and analyzers.
 
@@ -178,22 +225,21 @@ def dummy_event_data() -> RawEventData:
     pass
 
 
-def dummy_time_intervals() -> DirectBeamTimeIntervals[Cell, Spin]:
-    pass
-
-
-def dummy_sample_time_intervals() -> SampleTimeIntervals[PolarizerSpin, AnalyzerSpin]:
+def dummy_run_section() -> RunSection:
     pass
 
 
 providers = [
+    dummy_run_section,
+    dummy_cell_spin,
     dummy_event_data,
-    dummy_sample_time_intervals,
-    dummy_time_intervals,
     he3_direct_beam,
     he3_initial_atomic_polarization,
     he3_opacity,
     he3_transmission,
     polarization_corrected_sample_data,
-    sample_data,
+    sample_data_by_spin_channel,
+    spin_channel,
+    raw_data_by_run_section,
+    direct_beam_data_by_cell_and_polarization,
 ]
