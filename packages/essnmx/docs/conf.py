7c55b0d8cffe8bb4fb2eb7336ec26703852ac245
YooSunYoung <17974113+YooSunYoung@users.noreply.github.com>
2023-12-12 09:58:25 +0100

    Update from copier template.

--- packages/essnmx/docs/conf.py
+++ packages/essnmx/docs/conf.py
@@ -25,7 +25,7 @@ extensions = [
     'sphinx.ext.napoleon',
     'sphinx_autodoc_typehints',
     'sphinx_copybutton',
-    "sphinx_design",
+    'sphinx_design',
     'nbsphinx',
     'myst_parser',
 ]
@@ -152,7 +152,7 @@ html_theme_options = {
         },
         {
             "name": "Conda",
-            "url": "https://anaconda.org/conda-forge/essnmx",
+            "url": "https://anaconda.org/scipp/essnmx",
             "icon": "fa-custom fa-anaconda",
             "type": "fontawesome",
         },
@@ -191,8 +191,29 @@ nbsphinx_execute_arguments = [
 
 # -- Options for doctest --------------------------------------------------
 
+# sc.plot returns a Figure object and doctest compares that against the
+# output written in the docstring. But we only want to show an image of the
+# figure, not its `repr`.
+# In addition, there is no need to make plots in doctest as the documentation
+# build already tests if those plots can be made.
+# So we simply disable plots in doctests.
 doctest_global_setup = '''
 import numpy as np
+
+try:
+    import scipp as sc
+
+    def do_not_plot(*args, **kwargs):
+        pass
+
+    sc.plot = do_not_plot
+    sc.Variable.plot = do_not_plot
+    sc.DataArray.plot = do_not_plot
+    sc.DataGroup.plot = do_not_plot
+    sc.Dataset.plot = do_not_plot
+except ImportError:
+    # Scipp is not needed by docs if it is not installed.
+    pass
 '''
 
 # Using normalize whitespace because many __str__ functions in scipp produce
