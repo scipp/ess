6ccb847da890a7c185e9c59c3d6e72e4f2fe81d4
YooSunyoung <luysunyoung9@gmail.com>
2023-12-20 13:18:50 +0100

    Remove unused modules.

--- packages/essnmx/src/ess/nmx/workflow.py
+++
@@ -1,7 +0,0 @@
-# SPDX-License-Identifier: BSD-3-Clause
-# Copyright (c) 2023 Scipp contributors (https://github.com/scipp)
-from .detector import default_params as detector_default_params
-
-
-def collect_default_parameters() -> dict:
-    return dict(detector_default_params)
