3d1ad949585217837c6952485b911334d208cbba
Neil Vaytet <neil.vaytet@esss.se>
2023-12-20 16:36:25 +0100

    remove unused file

--- packages/esssans/src/esssans/loki/__init__.py
+++ packages/esssans/src/esssans/loki/__init__.py
@@ -6,8 +6,6 @@ from .general import default_parameters, providers as general_providers
 from .io import providers as io_providers
 from .masking import providers as masking_providers
 
-# from .utils import make_parameter_tables
-
 providers = general_providers + io_providers + masking_providers
 
 __all__ = ['providers', 'default_parameters', 'make_parameter_tables']
