d9f557a650a16e37942def393e19e6a57216feae
Simon Heybrock <simon.heybrock@ess.eu>
2024-01-30 10:30:21 +0100

    Add mechanism for setting sample and detector bank offset in Zoom workflow

--- packages/esssans/src/esssans/isis/__init__.py
+++ packages/esssans/src/esssans/isis/__init__.py
@@ -9,6 +9,7 @@ from ..sans2d.general import (
     sans2d_tube_detector_pixel_shape,
 )
 from . import io, masking
+from .components import DetectorBankOffset, SampleOffset, configure_raw_data
 from .io import CalibrationFilename, DataFolder, Filename, PixelMaskFilename
 from .masking import PixelMask
 from .visualization import plot_flat_detector_xy
@@ -19,6 +20,7 @@ providers = (
         get_monitor,
         lab_frame_transform,
         sans2d_tube_detector_pixel_shape,
+        configure_raw_data,
     )
     + io.providers
     + masking.providers
@@ -32,11 +34,22 @@ del sans2d_tube_detector_pixel_shape
 __all__ = [
     'CalibrationFilename',
     'DataFolder',
+    'DetectorBankOffset',
     'Filename',
+    'configure_raw_data',
     'io',
     'masking',
     'PixelMask',
     'PixelMaskFilename',
     'providers',
+<<<<<<< HEAD
+||||||| parent of 30d58f1 (Add mechanism for setting sample and detector bank offset in Zoom workflow)
+    'transmission_from_background_run',
+    'transmission_from_sample_run',
+=======
+    'SampleOffset',
+    'transmission_from_background_run',
+    'transmission_from_sample_run',
+>>>>>>> 30d58f1 (Add mechanism for setting sample and detector bank offset in Zoom workflow)
     'plot_flat_detector_xy',
 ]
---
+++ packages/esssans/src/esssans/isis/components.py
@@ -0,0 +1,42 @@
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright (c) 2024 Scipp contributors (https://github.com/scipp)
+from typing import NewType
+
+import sciline
+import scipp as sc
+
+from ..types import RawData, RunType
+
+PixelMask = NewType('PixelMask', sc.Variable)
+
+
+class ConfiguredRawData(sciline.Scope[RunType, sc.DataArray], sc.DataArray):
+    """Raw data with applied user configuration for component positions."""
+
+
+SampleOffset = NewType('SampleOffset', sc.Variable)
+DetectorBankOffset = NewType('DetectorBankOffset', sc.Variable)
+
+
+def configure_raw_data(
+    data: RawData[RunType],
+    sample_offset: SampleOffset,
+    detector_bank_offset: DetectorBankOffset,
+) -> ConfiguredRawData[RunType]:
+    """Apply user configuration to raw data.
+
+    Parameters
+    ----------
+    data:
+        Raw data.
+    sample_offset:
+        Sample offset.
+    detector_bank_offset:
+        Detector bank offset.
+    """
+    data = data.copy(deep=False)
+    sample_pos = data.coords['sample_position']
+    sample_pos += sample_offset.to(unit=sample_pos.unit, copy=False)
+    pos = data.coords['position']
+    pos += detector_bank_offset.to(unit=pos.unit, copy=False)
+    return ConfiguredRawData[RunType](data)
