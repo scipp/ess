4b1a0e4fd73a5dddd8d084b3b0f1d578399f01bf
Neil Vaytet <neil.vaytet@esss.se>
2024-02-01 15:09:50 +0100

    test for direct beam per layer and straw

--- packages/esssans/tests/loki/directbeam_test.py
+++ packages/esssans/tests/loki/directbeam_test.py
@@ -109,3 +109,32 @@ def test_can_compute_direct_beam_per_layer():
     assert iofq_bands.sizes['layer'] == 4
     assert direct_beam_function.sizes['wavelength'] == n_wavelength_bands
     assert direct_beam_function.sizes['layer'] == 4
+
+
+def test_can_compute_direct_beam_per_layer_and_straw():
+    n_wavelength_bands = 10
+    params = make_params()
+    params[WavelengthBands] = sc.linspace(
+        'wavelength',
+        params[WavelengthBins].min(),
+        params[WavelengthBins].max(),
+        n_wavelength_bands + 1,
+    )
+    params[DimsToKeep] = ['layer', 'straw']
+    providers = loki_providers()
+    pipeline = sciline.Pipeline(providers, params=params)
+    I0 = _get_I0(qbins=params[QBins])
+
+    results = sans.direct_beam(pipeline=pipeline, I0=I0, niter=4)
+    iofq_full = results[-1]['iofq_full']
+    iofq_bands = results[-1]['iofq_bands']
+    direct_beam_function = results[-1]['direct_beam']
+
+    assert iofq_full.dims == ('layer', 'straw', 'Q')
+    assert iofq_bands.dims == ('band', 'layer', 'straw', 'Q')
+    assert iofq_bands.sizes['band'] == n_wavelength_bands
+    assert iofq_bands.sizes['layer'] == 4
+    assert iofq_bands.sizes['straw'] == 7
+    assert direct_beam_function.sizes['wavelength'] == n_wavelength_bands
+    assert direct_beam_function.sizes['layer'] == 4
+    assert direct_beam_function.sizes['straw'] == 7
