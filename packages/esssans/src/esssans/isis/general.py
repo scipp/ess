bdf99d2a6d3271e6654a232a01c2f132155753e0
Neil Vaytet <neil.vaytet@esss.se>
2024-02-09 16:20:42 +0100

    add general providers

---
+++ packages/esssans/src/esssans/isis/general.py
@@ -0,0 +1,79 @@
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright (c) 2023 Scipp contributors (https://github.com/scipp)
+"""
+Providers for the ISIS instruments.
+"""
+import scipp as sc
+
+from ..types import (
+    DetectorPixelShape,
+    LabFrameTransform,
+    LoadedFileContents,
+    MonitorType,
+    NeXusMonitorName,
+    RawMonitor,
+    RunNumber,
+    RunTitle,
+    RunType,
+    SampleRun,
+    ScatteringRunType,
+)
+
+
+def get_monitor_data(
+    dg: LoadedFileContents[RunType], nexus_name: NeXusMonitorName[MonitorType]
+) -> RawMonitor[RunType, MonitorType]:
+    # See https://github.com/scipp/sciline/issues/52 why copy needed
+    mon = dg['monitors'][nexus_name]['data'].copy()
+    return RawMonitor[RunType, MonitorType](mon)
+
+
+def run_number(dg: LoadedFileContents[SampleRun]) -> RunNumber:
+    """Get the run number from the raw sample data."""
+    return RunNumber(int(dg['run_number']))
+
+
+def run_title(dg: LoadedFileContents[SampleRun]) -> RunTitle:
+    """Get the run title from the raw sample data."""
+    return RunTitle(dg['run_title'].value)
+
+
+def helium3_tube_detector_pixel_shape() -> DetectorPixelShape[ScatteringRunType]:
+    # Pixel radius and length
+    # found here:
+    # https://github.com/mantidproject/mantid/blob/main/instrument/SANS2D_Definition_Tubes.xml
+    R = 0.00405
+    L = 0.002033984375
+    pixel_shape = sc.DataGroup(
+        {
+            'vertices': sc.vectors(
+                dims=['vertex'],
+                values=[
+                    # Coordinates in pixel-local coordinate system
+                    # Bottom face center
+                    [0, 0, 0],
+                    # Bottom face edge
+                    [R, 0, 0],
+                    # Top face center
+                    [0, L, 0],
+                ],
+                unit='m',
+            ),
+            'nexus_class': 'NXcylindrical_geometry',
+        }
+    )
+    return pixel_shape
+
+
+def lab_frame_transform() -> LabFrameTransform[ScatteringRunType]:
+    # Rotate +y to -x
+    return sc.spatial.rotation(value=[0, 0, 1 / 2**0.5, 1 / 2**0.5])
+
+
+providers = (
+    get_monitor_data,
+    run_number,
+    run_title,
+    lab_frame_transform,
+    helium3_tube_detector_pixel_shape,
+)
