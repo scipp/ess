d3945c6dc2f212fcd2b009866829e26b0a3e0f99
Jan-Lukas Wynen <jan-lukas.wynen@ess.eu>
2024-02-14 16:13:28 +0100

    Add lorentz_factor

--- packages/essdiffraction/src/ess/diffraction/powder/correction.py
+++ packages/essdiffraction/src/ess/diffraction/powder/correction.py
@@ -44,3 +44,49 @@ def merge_calibration(*, into: sc.DataArray, calibration: sc.Dataset) -> sc.Data
         )
     out.masks['calibration'] = calibration['mask'].data
     return out
+
+
+def lorentz_factor(data: sc.DataArray) -> sc.DataArray:
+    """Compute the ToF powder diffraction Lorentz factor.
+
+    This function uses this definition:
+
+    .. math::
+
+        L = d^4 \\sin\\theta
+
+    where :math:`d` is the d-spacing, :math:`\\theta` is half the scattering angle
+    (note the definitions in
+    https://scipp.github.io/scippneutron/user-guide/coordinate-transformations.html).
+
+    The Lorentz factor as defined here is suitable for correcting time-of-flight data
+    expressed in wavelength or d-spacing.
+    It follows the definition used bty GSAS-II, see page 140 of
+    https://subversion.xray.aps.anl.gov/EXPGUI/gsas/all/GSAS%20Manual.pdf
+
+    Parameters
+    ----------
+    data:
+        Input data with coordinates ``two_theta`` and ``dspacing``.
+
+    Returns
+    -------
+    :
+        Powder Lorentz factor.
+    """
+
+    def f(dspacing: sc.Variable, two_theta: sc.Variable) -> sc.Variable:
+        return dspacing**4 * sc.sin(two_theta / 2)
+
+    aux = data.transform_coords(
+        'lorentz_factor', {'lorentz_factor': f}, rename_dims=False, quiet=True
+    )
+    if aux.bins is not None:
+        return aux.bins.coords['lorentz_factor']
+    return sc.DataArray(
+        aux.coords['lorentz_factor'],
+        coords={
+            'dspacing': data.coords['dspacing'],
+            'two_theta': data.coords['two_theta'],
+        },
+    )
--- packages/essdiffraction/tests/diffraction/powder/corrections_test.py
+++ packages/essdiffraction/tests/diffraction/powder/corrections_test.py
@@ -3,8 +3,10 @@
 import numpy as np
 import pytest
 import scipp as sc
+import scipp.testing
 
 from ess.diffraction.powder import merge_calibration
+from ess.diffraction.powder.correction import lorentz_factor
 
 
 @pytest.fixture
@@ -109,3 +111,102 @@ def test_merge_calibration_raises_if_mask_exists(calibration):
     )
     with pytest.raises(ValueError):
         merge_calibration(into=da, calibration=calibration)
+
+
+def test_lorentz_factor_dense_1d_coords():
+    da = sc.DataArray(
+        sc.ones(sizes={'detector_number': 3, 'dspacing': 4}),
+        coords={
+            'dspacing': sc.array(
+                dims=['dspacing'], values=[0.1, 0.4, 0.7, 1.1], unit='angstrom'
+            ),
+            'two_theta': sc.array(
+                dims=['detector_number'], values=[0.8, 0.9, 1.3], unit='rad'
+            ),
+            'detector_number': sc.array(
+                dims=['detector_number'], values=[0, 1, 2], unit=None
+            ),
+        },
+    )
+    factor = lorentz_factor(da)
+
+    assert factor.sizes == {'detector_number': 3, 'dspacing': 4}
+    assert factor.unit == 'angstrom**4'
+    assert factor.dtype == da.dtype
+    d = da.coords['dspacing'].broadcast(sizes=factor.sizes).values
+    two_theta = da.coords['two_theta'].broadcast(sizes=factor.sizes).values
+    np.testing.assert_allclose(factor.data.values, d**4 * np.sin(two_theta / 2))
+
+    assert set(factor.coords.keys()) == {'two_theta', 'dspacing'}
+    for key in factor.coords:
+        sc.testing.assert_identical(factor.coords[key], da.coords[key])
+
+
+def test_lorentz_factor_dense_2d_coord():
+    da = sc.DataArray(
+        sc.ones(sizes={'detector_number': 3, 'dspacing': 4}),
+        coords={
+            'dspacing': sc.array(
+                dims=['dspacing'], values=[0.1, 0.4, 0.7, 1.1], unit='angstrom'
+            ).broadcast(sizes={'detector_number': 3, 'dspacing': 4}),
+            'two_theta': sc.array(
+                dims=['detector_number'], values=[0.8, 0.9, 1.3], unit='rad'
+            ),
+            'detector_number': sc.array(
+                dims=['detector_number'], values=[0, 1, 2], unit=None
+            ),
+        },
+    )
+    factor = lorentz_factor(da)
+
+    assert factor.sizes == {'detector_number': 3, 'dspacing': 4}
+    assert factor.unit == 'angstrom**4'
+    assert factor.dtype == da.dtype
+    d = da.coords['dspacing'].values
+    two_theta = da.coords['two_theta'].broadcast(sizes=factor.sizes).values
+    np.testing.assert_allclose(factor.data.values, d**4 * np.sin(two_theta / 2))
+
+    assert set(factor.coords.keys()) == {'two_theta', 'dspacing'}
+    for key in factor.coords:
+        sc.testing.assert_identical(factor.coords[key], da.coords[key])
+
+
+def test_lorentz_factor_event_coords():
+    buffer = sc.DataArray(
+        sc.ones(sizes={'event': 6}),
+        coords={
+            'detector_number': sc.array(dims=['event'], values=[0, 3, 2, 2, 0, 4]),
+            'dspacing': sc.array(
+                dims=['event'], values=[0.1, 0.4, 0.2, 1.0, 1.3, 0.7], unit='angstrom'
+            ),
+            'two_theta': sc.array(
+                dims=['event'], values=[0.8, 1.2, 1.0, 1.0, 0.8, 0.7], unit='rad'
+            ),
+        },
+    )
+    da = buffer.group('detector_number').bin(dspacing=2)
+    factor = lorentz_factor(da)
+
+    assert factor.sizes == {'detector_number': 4, 'dspacing': 2}
+    assert factor.bins.unit == 'angstrom**4'
+    assert factor.bins.dtype == da.bins.dtype
+    d = buffer.coords['dspacing']
+    two_theta = buffer.coords['two_theta']
+    expected_buffer = sc.DataArray(d**4 * sc.sin(two_theta / 2), coords=buffer.coords)
+    expected = expected_buffer.group('detector_number').bin(dspacing=2)
+    np.testing.assert_allclose(
+        factor.bins.concat().value.values, expected.bins.concat().value.values
+    )
+
+
+def test_lorentz_factor_needs_coords():
+    da = sc.DataArray(
+        sc.ones(sizes={'detector_number': 3, 'dspacing': 4}),
+        coords={
+            'detector_number': sc.array(
+                dims=['detector_number'], values=[0, 1, 2], unit=None
+            )
+        },
+    )
+    with pytest.raises(KeyError):
+        lorentz_factor(da)
