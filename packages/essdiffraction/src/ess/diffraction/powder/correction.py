1d35a3ec61c2403e23e438be6c68de4c8c90f55d
Jan-Lukas Wynen <jan-lukas.wynen@ess.eu>
2024-02-14 16:14:02 +0100

    Mode docs

--- packages/essdiffraction/src/ess/diffraction/powder/correction.py
+++ packages/essdiffraction/src/ess/diffraction/powder/correction.py
@@ -1,5 +1,7 @@
 # SPDX-License-Identifier: BSD-3-Clause
 # Copyright (c) 2023 Scipp contributors (https://github.com/scipp)
+"""Correction algorithms for powder diffraction."""
+
 import scipp as sc
 
 
