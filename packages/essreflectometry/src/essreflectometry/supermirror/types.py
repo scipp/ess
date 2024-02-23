8ace7820206d15af0d81c23bad01b3aad7de8987
Johannes Kasimir <johanneskasimir@gmail.com>
2024-02-23 11:39:17 +0100

    docs: better docstrings for supermirror types

--- packages/essreflectometry/src/essreflectometry/supermirror/types.py
+++ packages/essreflectometry/src/essreflectometry/supermirror/types.py
@@ -2,9 +2,12 @@ from typing import NewType
 
 import scipp as sc
 
-# TODO What do they mean?
-# Supermirror parameters
+# TODO Better specification of what the supermirror parameters mean
 MValue = NewType('MValue', sc.Variable)
+''':math:`M` value of the supermirror'''
 CriticalEdge = NewType('CriticalEdge', sc.Variable)
+'''Critical edge value of the supermirror'''
 Alpha = NewType('Alpha', sc.Variable)
+''':math:`\\alpha` value of the supermirror'''
 SupermirrorCalibrationFactor = NewType('SupermirrorCalibrationFactor', sc.Variable)
+'''Calibration factor from the supermirror calibration'''
