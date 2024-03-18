1c0ab0c49272d1de0025499d29d25d3a6d54f937
Jan-Lukas Wynen <jan-lukas.wynen@ess.eu>
2024-03-18 13:48:02 +0100

    Support logical dims from NeXus

--- packages/essdiffraction/src/ess/dream/beamline.py
+++ packages/essdiffraction/src/ess/dream/beamline.py
@@ -3,26 +3,47 @@
 
 """Beamline tools for DREAM."""
 
-from ess.powder.types import DetectorDimensions, DetectorName
+from ess.powder.types import DetectorDimensions, RawDetectorData, SampleRun
 
 
-def dream_detector_dimensions(detector_name: DetectorName) -> DetectorDimensions:
+def dream_detector_dimensions(data: RawDetectorData[SampleRun]) -> DetectorDimensions:
     """Logical dimensions used by a DREAM detector.
+
+    The dimensions differ between simulated data loaded from GEANT4 CSV files
+    and measured data loaded from NeXus files.
+    The dimensions returned by this function match the dimensions found
+    in the ``data`` argument.
 
     Parameters
     ----------
-    detector_name:
-        Name of a detector of DREAM.
+    data:
+        Dimensions are deduced based on this data.
 
     Returns
     -------
     :
         Logical dimensions used by the given DREAM detector.
     """
-    base = ('module', 'segment', 'counter', 'wire', 'strip')
-    if detector_name == DetectorName.high_resolution:
-        return DetectorDimensions(base + ('sector',))
-    return DetectorDimensions(base)
+    geant4_dims = {
+        'module',
+        'segment',
+        'counter',
+        'wire',
+        'strip',
+        'sector',
+    }
+    nexus_dims = {
+        'wire',
+        'mounting_unit',
+        'cassette',
+        'counter',
+        'strip',
+        'sector',
+        'sumo_cass_ctr',
+        'other',
+    }
+    dims = (geant4_dims | nexus_dims) & set(data.dims)
+    return DetectorDimensions(tuple(dims))
 
 
 providers = (dream_detector_dimensions,)
