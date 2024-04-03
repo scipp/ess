a1916233e1b31c3aa4650b637757c1742c1f00c5
Neil Vaytet <neil.vaytet@esss.se>
2024-04-03 16:03:26 +0200

    isort

--- packages/esssans/tests/package_test.py
+++ packages/esssans/tests/package_test.py
@@ -1,7 +1,8 @@
 # SPDX-License-Identifier: BSD-3-Clause
 # Copyright (c) 2024 Scipp contributors (https://github.com/scipp)
 import pytest
-from ess import sans, loki, isissans
+
+from ess import isissans, loki, sans
 
 
 @pytest.mark.parametrize('pkg', [sans, loki, isissans])
