176cd640af76e149446033b1692305f0dd0558e5
YooSunyoung <luysunyoung9@gmail.com>
2024-04-16 14:09:15 +0200

    Hash variable tests.

--- packages/essnmx/tests/scaling_test.py
+++ packages/essnmx/tests/scaling_test.py
@@ -2,7 +2,7 @@
 # Copyright (c) 2024 Scipp contributors (https://github.com/scipp)
 import scipp as sc
 
-from ess.nmx.scaling import _apply_elem_wise
+from ess.nmx.scaling import _apply_elem_wise, hash_variable
 
 
 def test_apply_elem_wise_add() -> None:
@@ -30,3 +30,21 @@ def test_apply_elem_wise_vectors() -> None:
         _apply_elem_wise(sum, var),
         sc.array(dims=["x"], values=[6, 15, 24], dtype=float),
     )
+
+
+def test_hash_variable_unique() -> None:
+    """Different vector values should have different hashes."""
+    from itertools import product
+
+    import numpy as np
+
+    var = sc.vectors(dims=["x"], values=list(product(range(20), repeat=3)))
+    hash_var = hash_variable(var)
+    assert len(hash_var.values) == len(np.unique(hash_var.values))
+
+
+def test_hash_variable_same() -> None:
+    """Same values should have the same hash."""
+    var = sc.vectors(dims=["x"], values=[(1, 2, 3), (1, 2, 3)])
+    hash_var = hash_variable(var)
+    assert hash_var.values[0] == hash_var.values[1]
