be7d1fec933546bd6f494cec23bce015ec3fe235
YooSunyoung <luysunyoung9@gmail.com>
2024-04-23 16:10:51 +0200

    Update names and docstrings.

--- packages/essnmx/src/ess/nmx/scaling.py
+++ packages/essnmx/src/ess/nmx/scaling.py
@@ -1,36 +1,29 @@
 # SPDX-License-Identifier: BSD-3-Clause
 # Copyright (c) 2024 Scipp contributors (https://github.com/scipp)
-from typing import NewType
+from typing import NewType, Optional
 
 import scipp as sc
 
-from .mtz_io import DEFAULT_WAVELENGTH_COLUMN_NAME, NMXMtzDataArray
+from .mtz_io import DEFAULT_WAVELENGTH_COORD_NAME, NMXMtzDataArray
 
 # User defined or configurable types
 WavelengthBinSize = NewType("WavelengthBinSize", int)
 """The size of the wavelength(LAMBDA) bins."""
-
+ReferenceWavelength = NewType("ReferenceWavelength", sc.Variable)
+"""The wavelength to select reference intensities."""
 
 # Computed types
 WavelengthBinned = NewType("WavelengthBinned", sc.DataArray)
 """Binned mtz dataframe by wavelength(LAMBDA) with derived columns."""
-ReferenceWavelengthBin = NewType("ReferenceWavelengthBin", sc.DataArray)
-"""The reference bin in the binned dataset."""
-ReferenceScaleFactor = NewType("ReferenceScaleFactor", sc.DataArray)
-"""The reference scale factor, grouped by HKL_EQ."""
-ScaleFactorIntensity = NewType("ScaleFactorIntensity", float)
-"""The scale factor for intensity."""
-ScaleFactorSigmaIntensity = NewType("ScaleFactorSigmaIntensity", float)
-"""The scale factor for the standard uncertainty of intensity."""
-WavelengthScaled = NewType("WavelengthScaled", sc.DataArray)
-"""Scaled wavelength by the reference bin."""
+ReferenceIntensities = NewType("ReferenceIntensities", sc.DataArray)
+"""Reference intensities selected by the wavelength."""
+EstimatedScaleFactor = NewType("EstimatedScaleFactor", sc.DataArray)
+"""The estimated scale factor from the reference intensities per ``hkl_asu``."""
+EstimatedScaledIntensities = NewType("EstimatedScaledIntensities", float)
+"""Scaled intensities by the estimated scale factor."""
 
 
-def _is_bin_empty(binned: sc.DataArray, idx: int) -> bool:
-    return binned[idx].values.size == 0
-
-
-def get_lambda_binned(
+def get_wavelength_binned(
     mtz_da: NMXMtzDataArray,
     wavelength_bin_size: WavelengthBinSize,
 ) -> WavelengthBinned:
@@ -43,26 +36,19 @@ def get_lambda_binned(
     """
 
     return WavelengthBinned(
-        mtz_da.bin({DEFAULT_WAVELENGTH_COLUMN_NAME: wavelength_bin_size})
+        mtz_da.bin({DEFAULT_WAVELENGTH_COORD_NAME: wavelength_bin_size})
     )
 
 
-def get_reference_bin(binned: WavelengthBinned) -> ReferenceWavelengthBin:
-    """Find the reference group in the binned dataset.
+def _is_bin_empty(binned: sc.DataArray, idx: int) -> bool:
+    """Check if the bin is empty."""
+    return binned[idx].values.size == 0
 
-    The reference group is the group in the middle of the binned dataset.
-    If the middle group is empty, the function will search for the nearest.
 
-    Parameters
-    ----------
-    binned:
-        The wavelength binned data.
+def _get_middle_bin_idx(binned: WavelengthBinned) -> int:
+    """Find the middle bin index.
 
-    Raises
-    ------
-    ValueError:
-        If no reference group is found.
-
+    If the middle one is empty, the function will search for the nearest.
     """
     middle_number, offset = len(binned) // 2, 0
 
@@ -74,25 +60,98 @@ def get_reference_bin(binned: WavelengthBinned) -> ReferenceWavelengthBin:
     if _is_bin_empty(binned, cur_idx):
         raise ValueError("No reference group found.")
 
-    return binned[cur_idx].values.copy(deep=False)
+    return cur_idx
 
 
-def calculate_scale_factor_per_hkl_eq(
-    ref_bin: ReferenceWavelengthBin,
-) -> ReferenceScaleFactor:
+def get_reference_intensities(
+    binned: WavelengthBinned,
+    reference_wavelength: Optional[ReferenceWavelength] = None,
+) -> ReferenceIntensities:
+    """Find the reference intensities by the wavelength.
+
+    Parameters
+    ----------
+    binned:
+        The wavelength binned data.
+
+    reference_wavelength:
+        The reference wavelength to select the intensities.
+        If ``None``, the middle group is selected.
+        It should be a scalar variable as it is selecting one of bins.
+
+    Raises
+    ------
+    ValueError:
+        If no reference group is found.
+
+    """
+    if reference_wavelength is None:
+        ref_idx = _get_middle_bin_idx(binned)
+        return binned[ref_idx].values.copy(deep=False)
+    else:
+        if reference_wavelength.dims:
+            raise ValueError("Reference wavelength should be a scalar.")
+        try:
+            return binned['wavelength', reference_wavelength].values.copy(deep=False)
+        except IndexError:
+            raise IndexError(f"{reference_wavelength} out of range.")
+
+
+def estimate_scale_factor_per_hkl_asu_from_reference(
+    reference_intensities: ReferenceIntensities,
+) -> EstimatedScaleFactor:
+    """Calculate the estimated scale factor per ``hkl_asu``.
+
+    The estimated scale factor is calculatd as the average
+    of the inverse of the non-empty reference intensities.
+
+    It is part of the calculation of estimated scaled intensities
+    for fitting the scaling model.
+
+    .. math::
+
+        EstimatedScaleFactor_{(hkl)} = \\dfrac{
+            \\sum_{i=1}^{N_{(hkl)}} \\dfrac{1}{I_{i}}
+        }{
+            N_{(hkl)}
+        }
+        = average( \\dfrac{1}{I_{(hkl)}} )
+
+    Estimated scale factor is calculated per ``hkl_asu``.
+
+    Parameters
+    ----------
+    reference_intensities:
+        The reference intensities selected by wavelength.
+
+    Returns
+    -------
+    :
+        The estimated scale factor per ``hkl_asu``.
+        The result should have a dimension of ``hkl_asu``.
+
+        It does not have a dimension of ``wavelength`` since
+        it is calculated from the reference intensities,
+        which is selected by one ``wavelength``.
+
+    """
     # Workaround for https://github.com/scipp/scipp/issues/3046
-    grouped = ref_bin.group("H_EQ", "K_EQ", "L_EQ").flatten(
-        dims=["H_EQ", "K_EQ", "L_EQ"], to="HKL_EQ"
+    # and https://github.com/scipp/scipp/issues/3425
+    # This workaround is implemented with an assumption that
+    # the size of all combinations of (H_ASU, K_ASU, L_ASU) is small enough
+    # to be handled in memory.
+    grouped = reference_intensities.group("H_ASU", "K_ASU", "L_ASU").flatten(
+        dims=["H_ASU", "K_ASU", "L_ASU"], to="hkl_asu"
     )
     non_empty = grouped[grouped.bins.size().data > sc.scalar(0, unit=None)]
 
-    return ReferenceScaleFactor((1 / non_empty).bins.mean())
+    return EstimatedScaleFactor((1 / non_empty).bins.mean())
 
 
 # Providers and default parameters
 scaling_providers = (
-    get_lambda_binned,
-    get_reference_bin,
-    calculate_scale_factor_per_hkl_eq,
+    get_wavelength_binned,
+    get_reference_intensities,
+    estimate_scale_factor_per_hkl_asu_from_reference,
 )
 """Providers for scaling data."""
--- packages/essnmx/tests/scaling_test.py
+++ packages/essnmx/tests/scaling_test.py
@@ -5,9 +5,9 @@ import scipp as sc
 
 from ess.nmx.mtz_io import DEFAULT_WAVELENGTH_COLUMN_NAME
 from ess.nmx.scaling import (
-    ReferenceWavelengthBin,
-    calculate_scale_factor_per_hkl_eq,
-    get_reference_bin,
+    ReferenceIntensities,
+    estimate_scale_factor_per_hkl_asu_from_reference,
+    get_reference_intensities,
 )
 
 
@@ -19,9 +19,9 @@ def nmx_data_array() -> sc.DataArray:
             DEFAULT_WAVELENGTH_COLUMN_NAME: sc.Variable(
                 dims=["row"], values=[1, 2, 3, 4, 5, 3, 3]
             ),
-            "H_EQ": sc.array(dims=["row"], values=[1, 4, 7, 10, 13, 7, 9]),
-            "K_EQ": sc.array(dims=["row"], values=[2, 5, 8, 11, 14, 8, 8]),
-            "L_EQ": sc.array(dims=["row"], values=[3, 6, 9, 12, 15, 9, 7]),
+            "H_ASU": sc.array(dims=["row"], values=[1, 4, 7, 10, 13, 7, 9]),
+            "K_ASU": sc.array(dims=["row"], values=[2, 5, 8, 11, 14, 8, 8]),
+            "L_ASU": sc.array(dims=["row"], values=[3, 6, 9, 12, 15, 9, 7]),
         },
     )
     da.variances = (
@@ -33,7 +33,9 @@ def nmx_data_array() -> sc.DataArray:
 def test_get_reference_bin_middle(nmx_data_array: sc.DataArray) -> None:
     """Test the middle bin."""
 
-    ref_bin = get_reference_bin(nmx_data_array.bin({DEFAULT_WAVELENGTH_COLUMN_NAME: 6}))
+    ref_bin = get_reference_intensities(
+        nmx_data_array.bin({DEFAULT_WAVELENGTH_COLUMN_NAME: 6})
+    )
     selected_idx = (2, 5, 6)
     assert all(
         ref_bin.data.values == [nmx_data_array.data.values[idx] for idx in selected_idx]
@@ -41,19 +43,22 @@ def test_get_reference_bin_middle(nmx_data_array: sc.DataArray) -> None:
 
 
 @pytest.fixture
-def reference_bin(nmx_data_array: sc.DataArray) -> ReferenceWavelengthBin:
-    return get_reference_bin(nmx_data_array.bin({DEFAULT_WAVELENGTH_COLUMN_NAME: 6}))
+def reference_bin(nmx_data_array: sc.DataArray) -> ReferenceIntensities:
+    return get_reference_intensities(
+        nmx_data_array.bin({DEFAULT_WAVELENGTH_COLUMN_NAME: 6})
+    )
 
 
-def test_reference_bin_scale_factor(reference_bin: ReferenceWavelengthBin) -> None:
+def test_reference_bin_scale_factor(reference_bin: ReferenceIntensities) -> None:
     """Test the scale factor for I."""
-    scale_factor = calculate_scale_factor_per_hkl_eq(reference_bin)
+    scale_factor = estimate_scale_factor_per_hkl_asu_from_reference(reference_bin)
     expected_groups = [(7, 8, 9), (9, 8, 7)]
 
     assert len(scale_factor) == len(expected_groups)
+    assert scale_factor.dim == "hkl_asu"
     for idx, group in enumerate(expected_groups):
         hkl = tuple(
-            scale_factor.coords[coord][idx].value for coord in ("H_EQ", "K_EQ", "L_EQ")
+            scale_factor.coords[coord][idx].value
+            for coord in (f"{idx}_ASU" for idx in "HKL")
         )
-        print(hkl)
         assert hkl == group
