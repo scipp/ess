9bdb5ab2501f2649459fa9fff7165db5936f35f4
Johannes Kasimir <johanneskasimir@gmail.com>
2024-05-08 10:31:46 +0200

    docs: clean up comments

--- packages/essreflectometry/src/ess/amor/load.py
+++ packages/essreflectometry/src/ess/amor/load.py
@@ -33,44 +33,49 @@ def load_events(detector: RawDetector[Run]) -> RawEvents[Run]:
     return RawEvents[Run](data)
 
 
-class _Detector:
-    nBlades = sc.scalar(14)  # number of active blades in the detector
-    nWires = sc.scalar(32)  # number of wires per blade
-    nStripes = sc.scalar(64)  # number of stipes per blade
-    angle = sc.scalar(5.1, unit='degree').to(
-        unit='rad'
-    )  # deg  angle of incidence of the beam on the blades (def: 5.1)
-    dZ = sc.scalar(4.0, unit='mm') * sc.sin(
-        angle
-    )  # mm  height-distance of neighboring pixels on one blade
-    dX = sc.scalar(4.0, unit='mm') * sc.cos(
-        angle
-    )  # mm  depth-distance of neighboring pixels on one blace
-    bladeZ = sc.scalar(10.455, unit='mm')  # mm  distance between detector blades
-    zero = 0.5 * nBlades.value * bladeZ  # mm  vertical center of the detector
-    distance = sc.scalar(
-        4000, unit='mm'
-    )  # mm  distance from focal point to leading blade edge
+class Detector:
+    'Description of the geometry of the Amor detector'
+    # number of active blades in the detector
+    nBlades = sc.scalar(14)
+    # number of wires per blade
+    nWires = sc.scalar(32)
+    # number of stipes per blade
+    nStripes = sc.scalar(64)
+    # angle of incidence of the beam on the blades (def: 5.1)
+    angle = sc.scalar(5.1, unit='degree').to(unit='rad')
+    # height-distance of neighboring pixels on one blade
+    dZ = sc.scalar(4.0, unit='mm') * sc.sin(angle)
+    # depth-distance of neighboring pixels on one blade
+    dX = sc.scalar(4.0, unit='mm') * sc.cos(angle)
+    # distance between detector blades
+    bladeZ = sc.scalar(10.455, unit='mm')
+    # vertical center of the detector
+    zero = 0.5 * nBlades.value * bladeZ
+    # distance from focal point to leading blade edge
+    distance = sc.scalar(4000, unit='mm')
 
 
 def _pixel_coordinate_in_detector_system(pixelID):
     """determine spatial coordinates and angles from pixel number"""
     pixelID.unit = ''
-    (bladeNr, bPixel) = pixelID // (_Detector.nWires * _Detector.nStripes), pixelID % (
-        _Detector.nWires * _Detector.nStripes
+    (bladeNr, bPixel) = pixelID // (Detector.nWires * Detector.nStripes), pixelID % (
+        Detector.nWires * Detector.nStripes
     )
+    # z index on blade, y index on detector
     (bZi, detYi) = (
-        bPixel // _Detector.nStripes,
-        bPixel % _Detector.nStripes,
-    )  # z index on blade, y index on detector
-    detZi = bladeNr * _Detector.nWires + bZi  # z index on detector
-    detX = bZi * _Detector.dX  # x position in detector
+        bPixel // Detector.nStripes,
+        bPixel % Detector.nStripes,
+    )
+    # z index on detector
+    detZi = bladeNr * Detector.nWires + bZi
+    # x position in detector
+    detX = bZi * Detector.dX
 
-    bladeAngle = (2.0 * sc.asin(0.5 * _Detector.bladeZ / _Detector.distance)).to(
+    bladeAngle = (2.0 * sc.asin(0.5 * Detector.bladeZ / Detector.distance)).to(
         unit='degree'
     )
-    delta = (_Detector.nBlades / 2.0 - bladeNr) * bladeAngle - (
-        sc.atan(bZi * _Detector.dZ / (_Detector.distance + bZi * _Detector.dX))
+    delta = (Detector.nBlades / 2.0 - bladeNr) * bladeAngle - (
+        sc.atan(bZi * Detector.dZ / (Detector.distance + bZi * Detector.dX))
     ).to(unit='degree')
 
     # z is in the direction of the center of the beam, y is the direction 'up'
@@ -82,7 +87,7 @@ def _pixel_coordinate_in_lab_frame(pixelID, nu):
     _, detZi, detX, delta = _pixel_coordinate_in_detector_system(pixelID)
 
     angle_to_horizon = (nu + delta).to(unit='rad')
-    distance_to_pixel = detX + _Detector.distance
+    distance_to_pixel = detX + Detector.distance
     # TODO: put the correct value here
     global_X = sc.zeros(dims=pixelID.dims, shape=pixelID.shape, unit='mm')
     global_Y = distance_to_pixel * sc.sin(angle_to_horizon)
