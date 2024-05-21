120040774654260e706bcea0c55736770c3f811b
Johannes Kasimir <johanneskasimir@gmail.com>
2024-05-21 09:42:57 +0200

    refactor: reuse unique_child_group getter in essreduce

--- packages/essreflectometry/src/ess/reflectometry/load.py
+++ packages/essreflectometry/src/ess/reflectometry/load.py
@@ -1,4 +1,5 @@
-import h5py
+from typing import Optional, Type, Union
+
 import sciline
 import scipp as sc
 import scippnexus as snx
@@ -6,45 +7,43 @@ import scippnexus as snx
 from .types import IdealReferenceIntensity, ReferenceFilePath
 
 
-def _load(filepath, *paths: str):
-    with snx.File(filepath, 'r') as f:
-        for path in paths:
-            if path is not None:
-                yield f[path][()]
-            else:
-                yield None
+def load_nx(group: Union[snx.Group, str], *paths: str):
+    if isinstance(group, str):
+        with snx.File(group) as group:
+            yield from load_nx(group, *paths)
+        return
+    for path in paths:
+        g = group
+        for p in path.strip('/').split('/'):
+            g = (
+                _unique_child_group(g, getattr(snx, p), None)
+                if p.startswith('NX')
+                else g[p]
+            )
+        yield g[...]
 
 
-def _convert_nxpaths(filepath, *paths: str):
-    with h5py.File(filepath, 'r') as f:
-        for path in paths:
-            raw_path = []
-            current = f
-            for step in path.split('/'):
-                if step.startswith('NX'):
-                    for key, subgroup in current.items():
-                        if subgroup.attrs.get('NX_class') == step:
-                            current = subgroup
-                            raw_path.append(key)
-                            break
-                else:
-                    current = current.get(step)
-                    raw_path.append(step)
-                    if current is None:
-                        yield None
-                        break
-            else:
-                yield '/'.join(raw_path)
+# Remove when this function is exposed in essreduce
+def _unique_child_group(
+    group: snx.Group, nx_class: Type[snx.NXobject], name: Optional[str]
+) -> snx.Group:
+    if name is not None:
+        child = group[name]
+        if isinstance(child, snx.Field):
+            raise ValueError(
+                f"Expected a NeXus group as item '{name}' but got a field."
+            )
+        if child.nx_class != nx_class:
+            raise ValueError(
+                f"The NeXus group '{name}' was expected to be a "
+                f'{nx_class} but is a {child.nx_class}.'
+            )
+        return child
 
-
-def load_nx(filepath, *paths: str):
-    '''Helper that loads data from nexus file
-    Takes a number of paths of the form 'NXentry/NXuser/name'
-    where each part of the path can be either a NXclass or a key.
-    Any parts starting with 'NX' are assumed to be nexus classes
-    and all other parts are assumed to be keys.
-    '''
-    yield from _load(filepath, *_convert_nxpaths(filepath, *paths))
+    children = group[nx_class]
+    if len(children) != 1:
+        raise ValueError(f'Expected exactly one {nx_class} group, got {len(children)}')
+    return next(iter(children.values()))  # type: ignore[return-value]
 
 
 def save_reference(pl: sciline.Pipeline, fname: str):
