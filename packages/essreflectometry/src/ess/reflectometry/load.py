8fd9a7745174bce571bfcd3e5280c7f7e6fc156e
Johannes Kasimir <johanneskasimir@gmail.com>
2024-05-24 17:15:30 +0200

    docs: add comparison to Jochens software

--- packages/essreflectometry/src/ess/reflectometry/load.py
+++ packages/essreflectometry/src/ess/reflectometry/load.py
@@ -1,4 +1,4 @@
-from typing import Optional, Type, Union
+from typing import Type, Union
 
 import sciline
 import scipp as sc
@@ -16,32 +16,22 @@ def load_nx(group: Union[snx.Group, str], *paths: str):
         g = group
         for p in path.strip('/').split('/'):
             g = (
-                _unique_child_group(g, getattr(snx, p), None)
+                _unique_child_group(g, getattr(snx, p))
                 if p.startswith('NX')
-                else g[p]
+                else g.get(p)
             )
-        yield g[...]
+        yield g[...] if g is not None else None
 
 
 # Remove when this function is exposed in essreduce
 def _unique_child_group(
-    group: snx.Group, nx_class: Type[snx.NXobject], name: Optional[str]
+    group: snx.Group,
+    nx_class: Type[snx.NXobject],
 ) -> snx.Group:
-    if name is not None:
-        child = group[name]
-        if isinstance(child, snx.Field):
-            raise ValueError(
-                f"Expected a NeXus group as item '{name}' but got a field."
-            )
-        if child.nx_class != nx_class:
-            raise ValueError(
-                f"The NeXus group '{name}' was expected to be a "
-                f'{nx_class} but is a {child.nx_class}.'
-            )
-        return child
-
     children = group[nx_class]
-    if len(children) != 1:
+    if len(children) == 0:
+        return None
+    elif len(children) != 1:
         raise ValueError(f'Expected exactly one {nx_class} group, got {len(children)}')
     return next(iter(children.values()))  # type: ignore[return-value]
 
