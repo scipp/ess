9c3ee1ea3c334918a6704db0ffb8e5516cf10c7b
Johannes Kasimir <johanneskasimir@gmail.com>
2024-05-29 09:12:38 +0200

    test: footprint correction

--- packages/essreflectometry/tests/corrections_test.py
+++ packages/essreflectometry/tests/corrections_test.py
@@ -1,25 +1,29 @@
 # SPDX-License-Identifier: BSD-3-Clause
 # Copyright (c) 2023 Scipp contributors (https://github.com/scipp)
 import scipp as sc
+from scipp import constants as cst
+from scipp.testing import assert_allclose
 
 from ess.reflectometry import corrections
 
 
 def test_footprint_correction():
     data = sc.DataArray(
-        data=sc.array(['row'], values=[1.0], unit='counts'),
+        data=sc.array(dims=['row'], values=[1.0], unit='counts'),
         coords=dict(
-            theta=sc.array(['row'], values=[sc.pi / 6], unit='rad'),
-            px=sc.array(['row'], values=[1], unit=None),
+            theta=sc.array(dims=['row'], values=[cst.pi.value / 6], unit='rad'),
+            px=sc.array(dims=['row'], values=[1], unit=None),
         ),
-    ).bin(px=1)
-    out = corrections.footrint_correction(
+    ).group('px')
+    out = corrections.footprint_correction(
         data,
         beam_size=sc.scalar(5, unit='mm'),
         sample_size=sc.scalar(10, unit='mm'),
     )
-    sc.testing.assert_allclose(
-        out.data.value[0],
-        1
-        / sc.erf(1 / (sc.scalar(2) * sc.sqrt(sc.scalar(2.0) * sc.log(sc.scalar(2.0))))),
+    expected = sc.scalar(1, unit='counts') / sc.erf(
+        1 / (sc.scalar(2) * sc.sqrt(sc.scalar(2.0) * sc.log(sc.scalar(2.0))))
+    )
+    assert_allclose(
+        out.data.values[0].data[0],
+        expected,
     )
