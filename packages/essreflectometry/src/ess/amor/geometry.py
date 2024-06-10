1e490b1684145c6a2d75a0198bd53da2c06e8748
Neil Vaytet <neil.vaytet@esss.se>
2024-06-10 14:36:10 +0200

    flip z positions of detector pixels, and offset event_id by 1

--- packages/essreflectometry/src/ess/amor/geometry.py
+++ packages/essreflectometry/src/ess/amor/geometry.py
@@ -2,7 +2,8 @@ import scipp as sc
 
 
 class Detector:
-    'Description of the geometry of the Amor detector'
+    "Description of the geometry of the Amor detector"
+
     # number of active blades in the detector
     nBlades = sc.scalar(14)
     # number of wires per blade
@@ -10,24 +11,25 @@ class Detector:
     # number of stripes per blade
     nStripes = sc.scalar(64)
     # angle of incidence of the beam on the blades (def: 5.1)
-    angle = sc.scalar(5.1, unit='degree').to(unit='rad')
+    angle = sc.scalar(5.1, unit="degree").to(unit="rad")
     # height-distance of neighboring pixels on one blade
-    dZ = sc.scalar(4.0, unit='mm') * sc.sin(angle)
+    dZ = sc.scalar(4.0, unit="mm") * sc.sin(angle)
     # depth-distance of neighboring pixels on one blade
-    dX = sc.scalar(4.0, unit='mm') * sc.cos(angle)
+    dX = sc.scalar(4.0, unit="mm") * sc.cos(angle)
     # distance between detector blades
-    bladeZ = sc.scalar(10.455, unit='mm')
+    bladeZ = sc.scalar(10.455, unit="mm")
     # vertical center of the detector
     zero = 0.5 * nBlades.value * bladeZ
     # distance from focal point to leading blade edge
-    distance = sc.scalar(4000, unit='mm')
+    distance = sc.scalar(4000, unit="mm")
 
 
 def pixel_coordinate_in_detector_system(pixelID):
     """determine spatial coordinates and angles from pixel number"""
-    pixelID.unit = ''
-    (bladeNr, bPixel) = pixelID // (Detector.nWires * Detector.nStripes), pixelID % (
-        Detector.nWires * Detector.nStripes
+    pixelID.unit = ""
+    (bladeNr, bPixel) = (
+        pixelID // (Detector.nWires * Detector.nStripes),
+        pixelID % (Detector.nWires * Detector.nStripes),
     )
     # z index on blade, y index on detector
     (bZi, detYi) = (
@@ -37,14 +39,16 @@ def pixel_coordinate_in_detector_system(pixelID):
     # z index on detector
     detZi = bladeNr * Detector.nWires + bZi
     # x position in detector
-    detX = bZi * Detector.dX
+    # TODO: check with Jochen if this is correct, as old code was:
+    # detX = bZi * Detector.dX
+    detX = (Detector.nWires - 1 - bZi) * Detector.dX
 
     bladeAngle = (2.0 * sc.asin(0.5 * Detector.bladeZ / Detector.distance)).to(
-        unit='degree'
+        unit="degree"
     )
     delta = (Detector.nBlades / 2.0 - bladeNr) * bladeAngle - (
         sc.atan(bZi * Detector.dZ / (Detector.distance + bZi * Detector.dX))
-    ).to(unit='degree')
+    ).to(unit="degree")
 
     # z is in the direction of the center of the beam, y is the direction 'up'
     pixelID.unit = None
@@ -54,11 +58,12 @@ def pixel_coordinate_in_detector_system(pixelID):
 def pixel_coordinate_in_lab_frame(pixelID, nu):
     _, detZi, detX, delta = pixel_coordinate_in_detector_system(pixelID)
 
-    angle_to_horizon = (nu + delta).to(unit='rad')
+    angle_to_horizon = (nu + delta).to(unit="rad")
+    print("angle_to_horizon, nu", angle_to_horizon, nu)
     distance_to_pixel = detX + Detector.distance
 
     # TODO: put the correct value here
-    global_X = sc.zeros(dims=pixelID.dims, shape=pixelID.shape, unit='mm')
+    global_X = sc.zeros(dims=pixelID.dims, shape=pixelID.shape, unit="mm")
     global_Y = distance_to_pixel * sc.sin(angle_to_horizon)
     global_Z = distance_to_pixel * sc.cos(angle_to_horizon)
     return global_X, global_Y, global_Z, delta
--- packages/essreflectometry/src/ess/amor/load.py
+++ packages/essreflectometry/src/ess/amor/load.py
@@ -34,52 +34,52 @@ def load_events(
     detector: RawDetector[Run], detector_rotation: DetectorRotation[Run]
 ) -> RawEvents[Run]:
     detector_numbers = sc.arange(
-        'event_id',
-        start=0,
-        stop=(Detector.nBlades * Detector.nWires * Detector.nStripes).value,
+        "event_id",
+        start=1,
+        stop=(Detector.nBlades * Detector.nWires * Detector.nStripes).value + 1,
         unit=None,
-        dtype='int32',
+        dtype="int32",
     )
     data = (
         nexus.extract_detector_data(detector)
-        .bins.constituents['data']
+        .bins.constituents["data"]
         .group(detector_numbers)
         .fold(
-            'event_id',
+            "event_id",
             sizes={
-                'blade': Detector.nBlades,
-                'wire': Detector.nWires,
-                'stripe': Detector.nStripes,
+                "blade": Detector.nBlades,
+                "wire": Detector.nWires,
+                "stripe": Detector.nStripes,
             },
         )
     )
     # Recent versions of scippnexus no longer add variances for events by default, so
     # we add them here if they are missing.
-    if data.bins.constituents['data'].data.variances is None:
-        data.bins.constituents['data'].data.variances = data.bins.constituents[
-            'data'
+    if data.bins.constituents["data"].data.variances is None:
+        data.bins.constituents["data"].data.variances = data.bins.constituents[
+            "data"
         ].data.values
     x, y, z, angle_from_center_of_beam = pixel_coordinate_in_lab_frame(
-        data.coords['event_id'],
+        data.coords["event_id"] - sc.scalar(1, unit=None),
         detector_rotation,
     )
-    data.coords['position'] = sc.spatial.as_vectors(x, y, z).to(unit='m')
-    data.coords['angle_from_center_of_beam'] = angle_from_center_of_beam
+    data.coords["position"] = sc.spatial.as_vectors(x, y, z).to(unit="m")
+    data.coords["angle_from_center_of_beam"] = angle_from_center_of_beam
     return RawEvents[Run](data)
 
 
 def compute_tof(
     data: RawEvents[Run], phase: ChopperPhase[Run], frequency: ChopperFrequency[Run]
 ) -> ChopperCorrectedTofEvents[Run]:
-    data.bins.coords['tof'] = data.bins.coords.pop('event_time_offset').to(
-        unit='ns', dtype='float64', copy=False
+    data.bins.coords["tof"] = data.bins.coords.pop("event_time_offset").to(
+        unit="ns", dtype="float64", copy=False
     )
 
-    tof_unit = data.bins.coords['tof'].bins.unit
+    tof_unit = data.bins.coords["tof"].bins.unit
     tau = sc.to_unit(1 / (2 * frequency), tof_unit)
     tof_offset = tau * phase / (180.0 * sc.units.deg)
 
-    event_time_offset = data.bins.coords['tof']
+    event_time_offset = data.bins.coords["tof"]
 
     minimum = -tof_offset
     frame_bound = tau - tof_offset
@@ -94,66 +94,66 @@ def compute_tof(
             0.0 * tof_unit,
         ),
     )
-    data.bins.masks['outside_of_pulse'] = (minimum > event_time_offset) | (
+    data.bins.masks["outside_of_pulse"] = (minimum > event_time_offset) | (
         event_time_offset > maximum
     )
-    data.bins.coords['tof'] += offset
-    data.bins.coords['tof'] -= (
-        data.coords['angle_from_center_of_beam'] / (180.0 * sc.units.deg)
+    data.bins.coords["tof"] += offset
+    data.bins.coords["tof"] -= (
+        data.coords["angle_from_center_of_beam"] / (180.0 * sc.units.deg)
     ) * tau
     return ChopperCorrectedTofEvents[Run](data)
 
 
 def amor_chopper(f: FilePath[Run]) -> RawChopper[Run]:
-    return next(load_nx(f, 'NXentry/NXinstrument/NXdisk_chopper'))
+    return next(load_nx(f, "NXentry/NXinstrument/NXdisk_chopper"))
 
 
 def load_amor_chopper_1_position(ch: RawChopper[Run]) -> Chopper1Position[Run]:
     # We know the value has unit 'mm'
-    return sc.vector([0, 0, ch['distance'] - ch['pair_separation'] / 2], unit='mm').to(
-        unit='m'
+    return sc.vector([0, 0, ch["distance"] - ch["pair_separation"] / 2], unit="mm").to(
+        unit="m"
     )
 
 
 def load_amor_chopper_2_position(ch: RawChopper[Run]) -> Chopper2Position[Run]:
     # We know the value has unit 'mm'
-    return sc.vector([0, 0, ch['distance'] + ch['pair_separation'] / 2], unit='mm').to(
-        unit='m'
+    return sc.vector([0, 0, ch["distance"] + ch["pair_separation"] / 2], unit="mm").to(
+        unit="m"
     )
 
 
 def load_amor_ch_phase(ch: RawChopper[Run]) -> ChopperPhase[Run]:
-    p = ch['phase']['value'].coords['average_value'].value
-    if getattr(p, 'unit', None):
+    p = ch["phase"]["value"].coords["average_value"].value
+    if getattr(p, "unit", None):
         return p
-    raise ValueError('No unit was found for the chopper phase')
+    raise ValueError("No unit was found for the chopper phase")
 
 
 def load_amor_ch_frequency(ch: RawChopper[Run]) -> ChopperFrequency[Run]:
-    f = ch['rotation_speed']['value'].coords['average_value']
-    if getattr(f, 'unit', None):
+    f = ch["rotation_speed"]["value"].coords["average_value"]
+    if getattr(f, "unit", None):
         return f
-    raise ValueError('No unit was found for the chopper frequency')
+    raise ValueError("No unit was found for the chopper frequency")
 
 
 def load_amor_sample_rotation(fp: FilePath[Run]) -> SampleRotation[Run]:
-    (mu,) = load_nx(fp, 'NXentry/NXinstrument/master_parameters/mu')
+    (mu,) = load_nx(fp, "NXentry/NXinstrument/master_parameters/mu")
     # For some reason this is length 1 and not scalar sometimes
-    if mu['value'].coords['average_value'].dims != ():
-        mu = mu['value'].coords['average_value'].values[0]
+    if mu["value"].coords["average_value"].dims != ():
+        mu = mu["value"].coords["average_value"].values[0]
     else:
-        mu = mu['value'].coords['average_value'].value
-    return sc.scalar(mu, unit='deg')
+        mu = mu["value"].coords["average_value"].value
+    return sc.scalar(mu, unit="deg")
 
 
 def load_amor_detector_rotation(fp: FilePath[Run]) -> DetectorRotation[Run]:
-    (nu,) = load_nx(fp, 'NXentry/NXinstrument/master_parameters/nu')
+    (nu,) = load_nx(fp, "NXentry/NXinstrument/master_parameters/nu")
     # For some reason this is length 1 and not scalar sometimes
-    if nu['value'].coords['average_value'].dims != ():
-        nu = nu['value'].coords['average_value'].values[0]
+    if nu["value"].coords["average_value"].dims != ():
+        nu = nu["value"].coords["average_value"].values[0]
     else:
-        nu = nu['value'].coords['average_value'].value
-    return sc.scalar(nu, unit='deg')
+        nu = nu["value"].coords["average_value"].value
+    return sc.scalar(nu, unit="deg")
 
 
 providers = (
