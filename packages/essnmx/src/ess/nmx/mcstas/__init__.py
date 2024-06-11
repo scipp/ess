6ab5ccd96f0690559b4ceae05c43bfdb3578816e
Johannes Kasimir <johanneskasimir@gmail.com>
2024-06-11 14:54:02 +0200

    fixup

--- packages/essnmx/src/ess/nmx/mcstas/__init__.py
+++ packages/essnmx/src/ess/nmx/mcstas/__init__.py
@@ -1,16 +1,6 @@
 # SPDX-License-Identifier: BSD-3-Clause
 # Copyright (c) 2024 Scipp contributors (https://github.com/scipp)
 
-# flake8: noqa
-import importlib.metadata
-
-try:
-    __version__ = importlib.metadata.version(__package__ or __name__)
-except importlib.metadata.PackageNotFoundError:
-    __version__ = "0.0.0"
-
-del importlib
-
 
 def McStasWorkflow():
     import sciline as sl
