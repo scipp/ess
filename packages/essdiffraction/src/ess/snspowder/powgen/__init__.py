8bb4903b92af625b3405946d1043fa3054e9d0c6
Jan-Lukas Wynen <jan-lukas.wynen@ess.eu>
2024-06-19 17:05:57 +0200

    Add wrapper for fitting vanadium peaks

--- packages/essdiffraction/src/ess/snspowder/powgen/__init__.py
+++ packages/essdiffraction/src/ess/snspowder/powgen/__init__.py
@@ -7,7 +7,7 @@ Note that this module is temporary and will be removed in favor of
 the ``dream`` module when that is available.
 """
 
-from . import beamline, data
+from . import beamline, data, peaks
 from .calibration import load_calibration
 from .instrument_view import instrument_view
 from .workflow import PowgenWorkflow, default_parameters
@@ -26,5 +26,6 @@ __all__ = [
     'default_parameters',
     'instrument_view',
     'load_calibration',
+    'peaks',
     'providers',
 ]
