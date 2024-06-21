eb7401f8ab22f7b7022d8229b532e04eb1962e0e
Neil Vaytet <neil.vaytet@esss.se>
2024-06-21 15:05:01 +0200

    add powgen workflow

--- packages/essdiffraction/src/ess/snspowder/powgen/__init__.py
+++ packages/essdiffraction/src/ess/snspowder/powgen/__init__.py
@@ -7,6 +7,10 @@ Note that this module is temporary and will be removed in favor of
 the ``dream`` module when that is available.
 """
 
+import sciline
+
+from ess.powder import providers as powder_providers
+from ess.powder.types import NeXusDetectorName
 from . import beamline, data
 from .instrument_view import instrument_view
 
@@ -16,8 +20,24 @@ providers = (
 )
 """Sciline Providers for POWGEN-specific functionality."""
 
+
+def default_parameters() -> dict:
+    return {NeXusDetectorName: "powgen_detector"}
+
+
+def PowgenWorkflow() -> sciline.Pipeline:
+    """
+    Workflow with default parameters for the Powgen SNS instrument.
+    """
+    return sciline.Pipeline(
+        providers=powder_providers + providers, params=default_parameters()
+    )
+
+
 __all__ = [
+    'PowgenWorkflow',
     'beamline',
     'data',
+    'default_parameters',
     'instrument_view',
 ]
