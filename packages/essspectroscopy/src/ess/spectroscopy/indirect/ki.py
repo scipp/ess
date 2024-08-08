646127f354bd23486c9c194ed4614d075fe3c3aa
Gregory Tucker <gregory.tucker@ess.eu>
2024-08-08 15:55:40 +0200

    [Add] indirect ki tools

--- packages/essspectroscopy/src/ess/spectroscopy/indirect/ki.py
+++ packages/essspectroscopy/src/ess/spectroscopy/indirect/ki.py
@@ -2,30 +2,44 @@
 #TODO move elsewhere since possibly (probably) too specific to BIFROST.
 
 from ess.spectroscopy.types import *
+from scippnexus import Group
+
+def determine_name_with_type(instrument: Group, name: str | None, options: list, type_name: str) -> str:
+    if name is not None and name in instrument:
+        return name
+    foud = {x for x in instrument if type_name in x.lower()}
+    for option in options:
+        found.update(set(instrument[option]))
+    if len(found) != 1:
+        raise RuntimeError(f"Could not determine {type_name} name: {found}")
+    return list(found)[0]
 
 
-def source_position(file: NeXusFileName) -> SourcePosition:
-    from scippnexus import compute_positions, NXsource, NXmoderator, File
-    options = {}
+def guess_source_name(file: NeXusFileName) -> SourceName:
+    from scippnexus import NXsource, NXmoderator, File
     with File(file) as data:
         instrument = data['entry/instrument']
-        for nx_type in (NXsource, NXmoderator):
-            options.update(instrument[nx_type])
-    if len(options) == 0:
-        raise RuntimeError('No source position')
-    return compute_positions(list(options.values())[-1][...])['position']
+        return determine_name_with_type(instrument, name, [NXsource, NXmoderator], 'source')
 
-
-def sample_position(file: NeXusFileName) -> SamplePosition:
-    from scippnexus import compute_positions, NXsample, File
-    from scipp import vector
-    options = {}
+def guess_sample_name(file: NeXusFileName) -> SampleName:
+    from scippnexus import NXsample, File
     with File(file) as data:
         instrument = data['entry/instrument']
-        options.update(instrument[NXsample])
-    if len(options) == 0:
-        return vector(value=[0., 0., 0.], unit='m')
-    return compute_positions(list(options.values())[-1][...])['position']
+        return determine_name_with_type(instrument, name, [NXsample], 'sample')
+
+
+def source_position(file: NeXusFileName, source: SourceName) -> SourcePosition:
+    from scippnexus import compute_positions, File
+    with File(file) as data:
+        source = data['entry/instrument'][source][...]
+    return compute_positions(source)['position']
+
+
+def sample_position(file: NeXusFileName, sample: SampleName) -> SamplePosition:
+    from scippnexus import compute_positions, File
+    with File(file) as data:
+        sample = data['entry/instrument'][sample][...]
+    return compute_positions(sample)['position']
 
 
 def primary_path_length(file: NeXusFileName, source: SourcePosition, sample: SamplePosition) -> SourceSamplePathLength:
@@ -43,3 +57,26 @@ def primary_path_length(file: NeXusFileName, source: SourcePosition, sample: Sam
     positions = concat((source, *positions, sample), dim='path')
     diff = positions['path', 1:] - positions['path', :-1]
     return sum(sqrt(dot(diff, diff)))
+
+
+def primary_shortest_time(file: NeXusFileName,
+                          source: SourceName,
+                          sample: SampleName,
+                          frequency: SourceFrequency,
+                          duration: SourceDuration,
+                          delay: SourceDelay,
+                          velocities: SourceVelocities) -> SourceSampleFlightTime:
+    from scippnexus import File
+    from choppera.nexus import primary_shortest_time as choppera_primary_shortest_time
+    from choppera.nexus import primary_spectrometer
+    with File(file) as data:
+        instrument = data['entry/instrument']
+        assert source in instrument, f"The source '{source}' is not in the instrument group"
+        assert sample in instrument, f"The sample '{sample}' is not in the instrument group"
+        primary = primary_spectrometer(instrument, source, sample, frequency, duration, delay, velocities)
+        return choppera_primary_shortest_time(primary)
+
+
+def unwrap_sample_time(times: SampleFrameTime, frequency: SourceFrequency, least: SourceSampleFlightTime) -> SampleTime:
+    from choppera.nexus import unwrap as choppera_unwrap
+    return choppera_unwrap(times, frequency, least)
