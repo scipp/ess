4d1c38a1297d674a8deed4e7de9af7952fb8c55c
Gregory Tucker <gregory.tucker@ess.eu>
2024-09-14 21:32:02 +0200

    [Fix] instrument coordinate system: Y is vertical, not Z

--- packages/essspectroscopy/src/ess/spectroscopy/indirect/conservation.py
+++ packages/essspectroscopy/src/ess/spectroscopy/indirect/conservation.py
@@ -4,8 +4,8 @@ from .ki import providers as ki_providers
 from .kf import providers as kf_providers
 
 
-# scipp vectors are always float64 element type?
-X, Y, Z = [vector(v) for v in ([1, 0, 0], [0, 1, 0], [0, 0, 1])]
+# Directions relative to the incident beam coordinate system
+PERP, VERT, PARALLEL = [vector(v) for v in ([1, 0, 0], [0, 1, 0], [0, 0, 1])]
 
 
 def lab_momentum_vector(ki: IncidentWavevector, kf: FinalWavevector) -> LabMomentumTransfer:
@@ -14,17 +14,45 @@ def lab_momentum_vector(ki: IncidentWavevector, kf: FinalWavevector) -> LabMomen
 
 def lab_momentum_x(q: LabMomentumTransfer) -> LabMomentumTransferX:
     from scipp import dot
-    return dot(X, q)
+    return dot(PERP, q)
 
 
 def lab_momentum_y(q: LabMomentumTransfer) -> LabMomentumTransferY:
     from scipp import dot
-    return dot(Y, q)
+    return dot(VERT, q)
 
 
 def lab_momentum_z(q: LabMomentumTransfer) -> LabMomentumTransferZ:
     from scipp import dot
-    return dot(Z, q)
+    return dot(PARALLEL, q)
+
+
+def sample_table_momentum_vector(a3: SampleTableAngle, q: LabMomentumTransfer) -> TableMomentumTransfer:
+    """Rotate the momentum transfer vector into the sample-table coordinate system
+
+    When a3 is zero, the sample-table and lab coordinate systems are the same. That is, Z is along the incident
+    beam, Y is opposite the gravitational force, and X completes the right-handed coordinate system.
+    The sample-table angle, a3, has a rotation vector along Y, such that a positive 90-degree rotation places the
+    sample-table Z along the lab X.
+    """
+    from scipp.spatial import rotations_from_rotvecs
+    # negative a3 since we rotate coordinates not axes here
+    return rotations_from_rotvecs(-a3 * VERT) * q
+
+
+def sample_table_momentum_x(q: TableMomentumTransfer) -> TableMomentumTransferX:
+    from scipp import dot
+    return dot(PERP, q)
+
+
+def sample_table_momentum_y(q: TableMomentumTransfer) -> TableMomentumTransferY:
+    from scipp import dot
+    return dot(VERT, q)
+
+
+def sample_table_momentum_z(q: TableMomentumTransfer) -> TableMomentumTransferZ:
+    from scipp import dot
+    return dot(PARALLEL, q)
 
 
 # def energy(ei: IncidentEnergy, ef: FinalEnergy) -> EnergyTransfer:
@@ -43,5 +71,9 @@ providers = [
     lab_momentum_x,
     lab_momentum_y,
     lab_momentum_z,
+    sample_table_momentum_vector,
+    sample_table_momentum_x,
+    sample_table_momentum_y,
+    sample_table_momentum_z,
     energy,
 ]
