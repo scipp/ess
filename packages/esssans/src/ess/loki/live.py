6debdfcc4d44d3534da8951d22251e1c2ee7afb1
Simon Heybrock <simon.heybrock@ess.eu>
2024-10-10 09:30:51 +0200

    Cleanup

--- packages/esssans/src/ess/loki/live.py
+++ packages/esssans/src/ess/loki/live.py
@@ -50,15 +50,17 @@ RawDetectorView = NewType('RawDetectorView', sc.DataArray)
 
 
 def _raw_detector_view(data: DetectorData[SampleRun]) -> RawDetectorView:
+    """Very simple raw detector view for initial testing."""
+    # Instead of histogramming concrete x and y (which leads to artifacts), another
+    # quick option is to slice/sum some dimensions. But it will not give true positions:
+    # Option 1:
+    # return data.hist().sum(('straw', 'layer'))
+    # Option 2:
+    # return data['layer', 0].hist().sum('straw')
     da = data.hist()
     da.coords['x'] = da.coords['position'].fields.x.copy()
     da.coords['y'] = da.coords['position'].fields.y.copy()
     return da.hist(y=50, x=100)
-    return data.hist().sum(('straw', 'layer'))
-    return data['layer', 0].hist().sum('straw')
-
-
-_wavelength = sc.linspace("wavelength", 1.0, 13.0, 200 + 1, unit='angstrom')
 
 
 class GatheredMonitors(sciline.Scope[RunType, sc.DataGroup], sc.DataGroup): ...
@@ -68,6 +70,11 @@ def _gather_monitors(
     incident: WavelengthMonitor[RunType, Incident],
     transmission: WavelengthMonitor[RunType, Transmission],
 ) -> GatheredMonitors[RunType]:
+    """
+    Helper to allow for plotting multiple monitors in a single plot.
+
+    Might be better to handle this via a nested output spec in LiveWorkflow?
+    """
     return GatheredMonitors[RunType](
         sc.DataGroup(
             {'Incident Monitor': incident, 'Transmission Monitor': transmission}
@@ -75,17 +82,24 @@ def _gather_monitors(
     )
 
 
-def _configured_Larmor_AgBeh_workflow() -> sciline.Pipeline:
+def _configured_Larmor_workflow() -> sciline.Pipeline:
     wf = loki.LokiAtLarmorWorkflow()
     wf = with_pixel_mask_filenames(wf, masks=loki.data.loki_tutorial_mask_filenames())
     wf[CorrectForGravity] = True
     wf[UncertaintyBroadcastMode] = UncertaintyBroadcastMode.upper_bound
     wf[ReturnEvents] = False
 
-    wf[WavelengthBins] = _wavelength
+    wf[WavelengthBins] = sc.linspace('wavelength', 1.0, 13.0, 201, unit='angstrom')
     wf[QBins] = sc.linspace(dim='Q', start=0.01, stop=0.3, num=101, unit='1/angstrom')
     wf[QxBins] = sc.linspace(dim='Qx', start=-0.3, stop=0.3, num=61, unit='1/angstrom')
     wf[QyBins] = sc.linspace(dim='Qy', start=-0.3, stop=0.3, num=61, unit='1/angstrom')
+
+    wf[Filename[EmptyBeamRun]] = loki.data.loki_tutorial_run_60392()
+    return wf
+
+
+def _configured_Larmor_AgBeh_workflow() -> sciline.Pipeline:
+    wf = _configured_Larmor_workflow()
 
     # AgBeh
     wf[BeamCenter] = sc.vector(value=[-0.0295995, -0.02203635, 0.0], unit='m')
@@ -100,13 +114,12 @@ def _configured_Larmor_AgBeh_workflow() -> sciline.Pipeline:
     return wf
 
 
-class AccumulatorFactoryCreator:
+class AccumulatorFactories:
+    """Helper to create accumulator factories with different preprocessors."""
+
     def __init__(self, accum: type[streaming.Accumulator], **kwargs) -> None:
         self._accum = accum
         self._kwargs = kwargs
-
-    def make(self) -> streaming.Accumulator:
-        return self._accum(**self._kwargs)
 
     def with_hist(self) -> streaming.Accumulator:
         return self._accum(**self._kwargs, preprocess=streaming.maybe_hist)
@@ -115,12 +128,13 @@ class AccumulatorFactoryCreator:
         return self._accum(**self._kwargs, preprocess=lambda x: x.hist(wavelength=wav))
 
 
-def LokiMonitorWorkflow(nexus_filename: Path) -> LiveWorkflow:
+def make_monitor_workflow(
+    nexus_filename: Path, workflow: sciline.Pipeline
+) -> LiveWorkflow:
     """Loki monitor wavelength histogram workflow for live data reduction."""
-    workflow = loki.LokiAtLarmorWorkflow()
-    workflow[WavelengthBins] = _wavelength
-    creator = AccumulatorFactoryCreator(accum=streaming.RollingAccumulator, window=1)
-    factory = creator.with_wavelength_hist
+    # By adding accumulators we obtain automatic histogramming of our outputs.
+    factories = AccumulatorFactories(accum=streaming.RollingAccumulator, window=1)
+    factory = factories.with_wavelength_hist
     return LiveWorkflow.from_workflow(
         workflow=workflow,
         accumulators={
@@ -136,15 +150,13 @@ def LokiMonitorWorkflow(nexus_filename: Path) -> LiveWorkflow:
     )
 
 
-def LokiTransmissionRunWorkflow(nexus_filename: Path) -> LiveWorkflow:
+def make_transmission_run_workflow(
+    nexus_filename: Path, workflow: sciline.Pipeline
+) -> LiveWorkflow:
     """Loki transmission run workflow for live data reduction."""
-    workflow = loki.LokiAtLarmorWorkflow()
     workflow.insert(_gather_monitors)
-    workflow[WavelengthBins] = _wavelength
-    workflow[UncertaintyBroadcastMode] = UncertaintyBroadcastMode.upper_bound
-    workflow[Filename[EmptyBeamRun]] = loki.data.loki_tutorial_run_60392()
-    creator = AccumulatorFactoryCreator(accum=streaming.EternalAccumulator)
-    factory = creator.with_wavelength_hist
+    factories = AccumulatorFactories(accum=streaming.EternalAccumulator)
+    factory = factories.with_wavelength_hist
     return LiveWorkflow.from_workflow(
         workflow=workflow,
         accumulators={
@@ -160,9 +172,10 @@ def LokiTransmissionRunWorkflow(nexus_filename: Path) -> LiveWorkflow:
     )
 
 
-def LokiAtLarmorAgBehWorkflow(nexus_filename: Path) -> LiveWorkflow:
+def make_loki_workflow(
+    nexus_filename: Path, workflow: sciline.Pipeline
+) -> LiveWorkflow:
     """Loki workflow for live data reduction."""
-    workflow = _configured_Larmor_AgBeh_workflow()
     workflow.insert(_raw_detector_view)
     outputs = {'Raw Detector': RawDetectorView}
     try:
@@ -172,18 +185,33 @@ def LokiAtLarmorAgBehWorkflow(nexus_filename: Path) -> LiveWorkflow:
     else:
         iofq_keys = (BackgroundSubtractedIofQ, BackgroundSubtractedIofQxy)
     outputs.update(dict(zip(('I(Q)', '$I(Q_x, Q_y)$'), iofq_keys, strict=True)))
-    factory = AccumulatorFactoryCreator(accum=streaming.RollingAccumulator, window=20)
+    factories = AccumulatorFactories(accum=streaming.RollingAccumulator, window=20)
 
     return LiveWorkflow.from_workflow(
         workflow=workflow,
         accumulators={
-            ReducedQ[SampleRun, Numerator]: factory.with_hist,
-            ReducedQ[SampleRun, Denominator]: factory.with_hist,
-            ReducedQxy[SampleRun, Numerator]: factory.with_hist,
-            ReducedQxy[SampleRun, Denominator]: factory.with_hist,
-            RawDetectorView: factory.make,
+            ReducedQ[SampleRun, Numerator]: factories.with_hist,
+            ReducedQ[SampleRun, Denominator]: factories.with_hist,
+            ReducedQxy[SampleRun, Numerator]: factories.with_hist,
+            ReducedQxy[SampleRun, Denominator]: factories.with_hist,
+            RawDetectorView: factories.with_hist,
         },
         outputs=outputs,
         run_type=SampleRun,
         nexus_filename=nexus_filename,
     )
+
+
+def LokiMonitorTestWorkflow(nexus_filename: Path) -> LiveWorkflow:
+    """Fully preconfigured monitor workflow for, testing with Beamlime."""
+    return make_monitor_workflow(nexus_filename, _configured_Larmor_workflow())
+
+
+def LokiTransmissionRunTestWorkflow(nexus_filename: Path) -> LiveWorkflow:
+    """Fully preconfigured transmission run workflow, for testing with Beamlime."""
+    return make_transmission_run_workflow(nexus_filename, _configured_Larmor_workflow())
+
+
+def LokiAtLarmorAgBehTestWorkflow(nexus_filename: Path) -> LiveWorkflow:
+    """Fully preconfigured I(Q) workflow for AgBeh, for testing with Beamlime."""
+    return make_loki_workflow(nexus_filename, _configured_Larmor_AgBeh_workflow())
--- packages/esssans/tests/loki/live_reduction_test.py
+++ packages/esssans/tests/loki/live_reduction_test.py
@@ -6,17 +6,17 @@ import scipp as sc
 import scippnexus as snx
 
 from ess.loki import data
-from ess.loki.live import LokiMonitorWorkflow
+from ess.loki.live import LokiMonitorTestWorkflow
 from ess.reduce.nexus.json_generator import event_data_generator
 from ess.reduce.nexus.json_nexus import JSONGroup
 
 
 def test_can_create_loki_monitor_workflow() -> None:
     filename = data.loki_tutorial_sample_run_60250()
-    _ = LokiMonitorWorkflow(filename)
+    _ = LokiMonitorTestWorkflow(filename)
 
 
-def _call_workflow(wf: LokiMonitorWorkflow, **kwargs: dict) -> sc.DataGroup:
+def _call_workflow(wf: LokiMonitorTestWorkflow, **kwargs: dict) -> sc.DataGroup:
     nxevent_data = {
         f'/entry/instrument/{k}/{k}_events': JSONGroup(v) for k, v in kwargs.items()
     }
@@ -29,7 +29,7 @@ def test_workflow_processes_monitor_event_data_chunks() -> None:
     mon2 = snx.load(filename, root='entry/instrument/monitor_2/monitor_2_events')
     generator1 = event_data_generator(mon1)
     generator2 = event_data_generator(mon2)
-    wf = LokiMonitorWorkflow(filename)
+    wf = LokiMonitorTestWorkflow(filename)
 
     group1 = next(generator1)
     group2 = next(generator2)
@@ -53,7 +53,7 @@ def test_workflow_raises_if_event_data_missing() -> None:
     mon2 = snx.load(filename, root='entry/instrument/monitor_2/monitor_2_events')
     generator1 = event_data_generator(mon1)
     generator2 = event_data_generator(mon2)
-    wf = LokiMonitorWorkflow(filename)
+    wf = LokiMonitorTestWorkflow(filename)
 
     result = _call_workflow(wf, monitor_1=next(generator1), monitor_2=next(generator2))
     assert list(result) == ['Incident Monitor', 'Transmission Monitor']
@@ -68,7 +68,7 @@ def test_workflow_raises_if_event_data_not_updated() -> None:
     mon2 = snx.load(filename, root='entry/instrument/monitor_2/monitor_2_events')
     generator1 = event_data_generator(mon1)
     generator2 = event_data_generator(mon2)
-    wf = LokiMonitorWorkflow(filename)
+    wf = LokiMonitorTestWorkflow(filename)
 
     result = _call_workflow(wf, monitor_1=next(generator1), monitor_2=next(generator2))
     assert list(result) == ['Incident Monitor', 'Transmission Monitor']
@@ -87,7 +87,7 @@ def test_workflow_ignores_extra_event_data() -> None:
     mon2 = snx.load(filename, root='entry/instrument/monitor_2/monitor_2_events')
     generator1 = event_data_generator(mon1)
     generator2 = event_data_generator(mon2)
-    wf = LokiMonitorWorkflow(filename)
+    wf = LokiMonitorTestWorkflow(filename)
 
     result = _call_workflow(
         wf,
