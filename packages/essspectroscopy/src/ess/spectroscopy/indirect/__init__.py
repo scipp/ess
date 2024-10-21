751d545d1eeb6ecb5407218afaeb8561edfad2e4
Gregory Tucker <gregory.tucker@ess.eu>
2024-10-21 15:51:43 +0200

    [Add] NXspe output and relegation ADR

--- packages/essspectroscopy/src/ess/spectroscopy/indirect/__init__.py
+++ packages/essspectroscopy/src/ess/spectroscopy/indirect/__init__.py
@@ -1,9 +1,10 @@
 # SPDX-License-Identifier: BSD-3-Clause
 # Copyright (c) 2024 Scipp contributors (https://github.com/scipp)
 
-from .workflow import bifrost, bifrost_single
+from .workflow import bifrost, bifrost_single, bifrost_to_nxspe
 
 __all__ = (
     "bifrost",
     "bifrost_single",
+    "bifrost_to_nxspe",
 )
