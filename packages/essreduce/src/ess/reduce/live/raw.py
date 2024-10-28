eff07981f9f8a4bbc414612bf54a6537fd43d549
Simon Heybrock <simon.heybrock@ess.eu>
2024-10-28 09:20:17 +0100

    Cleanup and docs

--- packages/essreduce/src/ess/reduce/live/raw.py
+++ packages/essreduce/src/ess/reduce/live/raw.py
@@ -1,6 +1,23 @@
 # SPDX-License-Identifier: BSD-3-Clause
 # Copyright (c) 2024 Scipp contributors (https://github.com/scipp)
-"""Raw count processing and visualization for live data display."""
+"""
+Raw count processing and visualization for live data display.
+
+The main feature of this module if :py:class:`RollingDetectorView`, which provides a
+rolling view of a detector. This is useful for live data display, where the most recent
+counts, i.e., from the last N pulses, are of interest.
+
+As various detector geometries can often not be directly visualized in a useful way,
+the module also provides a way to project the detector data onto a different coordinate
+systems. A full set of options is not implemented yet. Currently there are three
+options:
+
+- `'xy_plane'`: Project the data onto the x-y plane, i.e., perpendicular to the beam.
+- `'cylinder_mantle_z'`: Project the data onto the mantle of a cylinder aligned with the
+   z-axis.
+- `LogicalView`: Not a projection in the traditional sense, but a way to select and
+  flatten dimensions of the data.
+"""
 
 from collections.abc import Callable, Sequence
 from dataclasses import dataclass, field
@@ -20,6 +37,72 @@ from ess.reduce.nexus.types import (
     SampleRun,
 )
 from ess.reduce.nexus.workflow import GenericNeXusWorkflow
+
+CalibratedPositionWithNoisyReplicas = NewType(
+    'CalibratedPositionWithNoisyReplicas', sc.Variable
+)
+DetectorViewResolution = NewType('DetectorViewResolution', dict[str, int])
+PixelCylinderAxis = NewType('PixelCylinderAxis', sc.Variable)
+PixelCylinderRadius = NewType('PixelCylinderRadius', sc.Variable)
+PixelShape = NewType('PixelShape', sc.DataGroup)
+PositionNoise = NewType('PositionNoise', sc.Variable)
+PositionNoiseReplicaCount = NewType('PositionNoiseReplicaCount', int)
+PositionNoiseSigma = NewType('PositionNoiseSigma', sc.Variable)
+ProjectedCoords = NewType('ProjectedCoords', sc.DataGroup[str, sc.Variable])
+RollingDetectorViewWindow = NewType('RollingDetectorViewWindow', int)
+
+
+class Histogrammer:
+    """
+    Histogrammer for a detector view.
+
+    The histogrammer is used to project the detector data onto a different coordinate
+    system. This class implements the common logic for different projections. For each
+    concrete projection a setup mechanism computes relevant coordinates. This is done,
+    e.g., in :py:func:`project_xy` and :py:func:`project_onto_cylinder`.
+    """
+
+    def __init__(
+        self,
+        coords: sc.DataGroup[str, sc.Variable],
+        edges: sc.DataGroup[str, sc.Variable],
+    ):
+        self._current = 0
+        self._replica_dim = 'replica'
+        self._replicas = coords.sizes[self._replica_dim]
+        self._coords = coords
+        self._edges = edges
+
+    @staticmethod
+    def from_coords(
+        coords: ProjectedCoords, resolution: DetectorViewResolution
+    ) -> 'Histogrammer':
+        """
+        Create a histogrammer from coordinates and resolution.
+
+        Parameters
+        ----------
+        coords:
+            Coordinates to use for histogramming. May contain more than the
+            dimensions used for histogramming.
+        resolution:
+            Resolution to use for histogramming. The keys are the dimensions
+            to histogram, and the values are the number of bins to use. The order
+            of the dimensions is preserved in the output and thus controls which axis
+            is which in a plot.
+        """
+        edges = sc.DataGroup(
+            {
+                dim: coords[dim].hist({dim: res}).coords[dim]
+                for dim, res in resolution.items()
+            }
+        )
+        return Histogrammer(coords=coords, edges=edges)
+
+    def __call__(self, da: sc.DataArray) -> sc.DataArray:
+        self._current += 1
+        coords = self._coords[self._replica_dim, self._current % self._replicas]
+        return sc.DataArray(da.data, coords=coords).hist(self._edges)
 
 
 @dataclass
@@ -80,6 +163,13 @@ class Detector:
 
 
 class RollingDetectorView(Detector):
+    """
+    Rolling view of a detector.
+
+    The view keeps a history of counts and allows to access the sum of counts over a
+    window of the most recent counts.
+    """
+
     def __init__(
         self,
         *,
@@ -87,6 +177,21 @@ class RollingDetectorView(Detector):
         window: int,
         projection: Callable[[sc.DataArray], sc.DataArray] | None = None,
     ):
+        """
+        Create a rolling detector view.
+
+        Parameters
+        ----------
+        detector_number:
+            Detector number for each pixel. This is used for mapping event_id to pixel.
+        window:
+            Size of the rolling window. This is the maximum window size, smaller windows
+            can be accessed using the `get` method.
+        projection:
+            Optional projection function to apply to the counts before storing them in
+            the history. This can be used to project the data onto a different
+            coordinate system or to reduce the dimensionality of the data.
+        """
         super().__init__(detector_number=detector_number)
         self._projection = projection
         self._window = window
@@ -105,33 +210,86 @@ class RollingDetectorView(Detector):
         self._cache = self._history.sum('window')
 
     @staticmethod
+    def from_detector_and_histogrammer(
+        detector: CalibratedDetector[SampleRun],
+        window: RollingDetectorViewWindow,
+        projection: Histogrammer,
+    ) -> 'RollingDetectorView':
+        """Helper for constructing via a Sciline workflow."""
+        return RollingDetectorView(
+            detector_number=detector.coords['detector_number'],
+            window=window,
+            projection=projection,
+        )
+
+    @staticmethod
+    def from_detector_and_logical_view(
+        detector: CalibratedDetector[SampleRun],
+        window: RollingDetectorViewWindow,
+        projection: LogicalView,
+    ) -> 'RollingDetectorView':
+        """Helper for constructing via a Sciline workflow."""
+        return RollingDetectorView(
+            detector_number=detector.coords['detector_number'],
+            window=window,
+            projection=projection,
+        )
+
+    @staticmethod
     def from_nexus(
         nexus_file: str,
         *,
         detector_name: str,
         window: int,
         projection: Literal['xy_plane', 'cylinder_mantle_z'] | LogicalView,
-        resolution: dict[str, int],
+        resolution: dict[str, int] | None = None,
         pixel_noise: Literal['cylindrical'] | sc.Variable | None = None,
     ) -> 'RollingDetectorView':
+        """
+        Create a rolling detector view from a NeXus file using GenericNeXusWorkflow.
+
+        The configuration parameters are preliminary for testing and are expected to
+        change in the future.
+
+        Parameters
+        ----------
+        nexus_file:
+            NeXus file providing detector parameters such as detector_number and pixel
+            positions.
+        detector_name:
+            Name of the detector in the NeXus file.
+        window:
+            Size of the rolling window.
+        projection:
+            Projection to use for the detector data. This can be a string selecting a
+            predefined projection or a LogicalView instance.
+        resolution:
+            Resolution to use for histogramming the detector data. Not required when the
+            projection is a LogicalView.
+        pixel_noise:
+            Noise to add to the pixel positions. This can be a scalar value to add
+            Gaussian noise to the pixel positions or the string 'cylindrical' to add
+            noise to the pixel positions of a cylindrical detector. Adding noise can be
+            useful to avoid artifacts when projecting the data.
+        """
         if pixel_noise is None:
             pixel_noise = sc.scalar(0.0, unit='m')
             noise_replica_count = 0
         else:
             noise_replica_count = 4
         wf = GenericNeXusWorkflow()
-        if projection == 'cylinder_mantle_z':
+        wf[RollingDetectorViewWindow] = window
+        if isinstance(projection, LogicalView):
+            wf[LogicalView] = projection
+            wf.insert(RollingDetectorView.from_detector_and_logical_view)
+        elif projection == 'cylinder_mantle_z':
             wf.insert(make_cylinder_mantle_coords)
-            wf.insert(make_rolling_detector_view_factory(window=window))
+            wf.insert(RollingDetectorView.from_detector_and_histogrammer)
+            wf[DetectorViewResolution] = resolution
         elif projection == 'xy_plane':
             wf.insert(make_xy_plane_coords)
-            wf.insert(make_rolling_detector_view_factory(window=window))
-        elif isinstance(projection, LogicalView):
-            wf.insert(
-                make_rolling_logical_detector_view_factory(
-                    window=window, selection=projection
-                )
-            )
+            wf.insert(RollingDetectorView.from_detector_and_histogrammer)
+            wf[DetectorViewResolution] = resolution
         else:
             raise ValueError(f"Invalid {projection=}.")
         if isinstance(pixel_noise, sc.Variable):
@@ -149,7 +307,6 @@ class RollingDetectorView(Detector):
         wf[PositionNoiseReplicaCount] = noise_replica_count
         wf[Filename[SampleRun]] = nexus_file
         wf[NeXusDetectorName] = detector_name
-        wf[DetectorViewResolution] = resolution
         return wf.compute(RollingDetectorView)
 
     def get(self, window: int | None = None) -> sc.DataArray:
@@ -179,6 +336,11 @@ class RollingDetectorView(Detector):
 def project_xy(
     position: sc.Variable, *, zplane: sc.Variable | None = None
 ) -> dict[str, sc.Variable]:
+    """
+    Project positions onto the x-y plane, i.e., perpendicular to the beam (z axis).
+
+    This is useful, e.g., for SANS instruments.
+    """
     z = position.fields.z
     zmin = z.min()
     zero = sc.zeros_like(zmin)
@@ -191,6 +353,11 @@ def project_xy(
 def project_onto_cylinder(
     position: sc.Variable, *, radius: sc.Variable | None = None
 ) -> dict[str, sc.Variable]:
+    """
+    Project positions onto the mantle of a cylinder aligned with the z axis.
+
+    This is useful for cylindrical detectors, provided they are aligned along the beam.
+    """
     x = position.fields.x
     y = position.fields.y
     r_xy = sc.sqrt(x**2 + y**2)
@@ -200,89 +367,6 @@ def project_onto_cylinder(
     phi = sc.atan2(y=y, x=x).to(unit='deg')
     arclength = radius * (phi * sc.scalar(np.pi / 180.0, unit='1/deg'))
     return sc.DataGroup(phi=phi, r=radius, z=position.fields.z * t, arclength=arclength)
-
-
-PixelShape = NewType('PixelShape', sc.DataGroup)
-PixelCylinderAxis = NewType('PixelCylinderAxis', sc.Variable)
-PixelCylinderRadius = NewType('PixelCylinderRadius', sc.Variable)
-PositionNoise = NewType('PositionNoise', sc.Variable)
-PositionNoiseReplicaCount = NewType('PositionNoiseReplicaCount', int)
-CalibratedPositionWithNoisyReplicas = NewType(
-    'CalibratedPositionWithNoisyReplicas', sc.Variable
-)
-PositionNoiseSigma = NewType('PositionNoiseSigma', sc.Variable)
-ProjectedCoords = NewType('ProjectedCoords', sc.DataGroup[str, sc.Variable])
-DetectorViewResolution = NewType('DetectorViewResolution', dict[str, int])
-
-
-class Histogrammer:
-    def __init__(
-        self,
-        coords: sc.DataGroup[str, sc.Variable],
-        edges: sc.DataGroup[str, sc.Variable],
-    ):
-        self._current = 0
-        self._replica_dim = 'replica'
-        self._replicas = coords.sizes[self._replica_dim]
-        self._coords = coords
-        self._edges = edges
-
-    @staticmethod
-    def from_coords(
-        coords: ProjectedCoords, resolution: DetectorViewResolution
-    ) -> 'Histogrammer':
-        """
-        Create a histogrammer from coordinates and resolution.
-
-        Parameters
-        ----------
-        coords:
-            Coordinates to use for histogramming. May contain more than the
-            dimensions used for histogramming.
-        resolution:
-            Resolution to use for histogramming. The keys are the dimensions
-            to histogram, and the values are the number of bins to use. The order
-            of the dimensions is preserved in the output and thus controls which axis
-            is which in a plot.
-        """
-        edges = sc.DataGroup(
-            {
-                dim: coords[dim].hist({dim: res}).coords[dim]
-                for dim, res in resolution.items()
-            }
-        )
-        return Histogrammer(coords=coords, edges=edges)
-
-    def __call__(self, da: sc.DataArray) -> sc.DataArray:
-        self._current += 1
-        coords = self._coords[self._replica_dim, self._current % self._replicas]
-        return sc.DataArray(da.data, coords=coords).hist(self._edges)
-
-
-def make_rolling_logical_detector_view_factory(window: int, selection: LogicalView):
-    def make_rolling_detector_view(
-        detector: CalibratedDetector[SampleRun],
-    ) -> RollingDetectorView:
-        return RollingDetectorView(
-            detector_number=detector.coords['detector_number'],
-            window=window,
-            projection=selection,
-        )
-
-    return make_rolling_detector_view
-
-
-def make_rolling_detector_view_factory(window: int):
-    def make_rolling_detector_view(
-        detector: CalibratedDetector[SampleRun], projection: Histogrammer
-    ) -> RollingDetectorView:
-        return RollingDetectorView(
-            detector_number=detector.coords['detector_number'],
-            window=window,
-            projection=projection,
-        )
-
-    return make_rolling_detector_view
 
 
 def pixel_shape(component: NeXusComponent[snx.NXdetector, SampleRun]) -> PixelShape:
