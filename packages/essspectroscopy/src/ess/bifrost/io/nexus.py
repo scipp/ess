0414b49db0bebaff3acf64de1f33317e63e0beb7
Jan-Lukas Wynen <jan-lukas.wynen@ess.eu>
2024-11-22 14:22:29 +0100

    Select moderator via its class

--- packages/essspectroscopy/src/ess/bifrost/io/nexus.py
+++ packages/essspectroscopy/src/ess/bifrost/io/nexus.py
@@ -7,26 +7,19 @@ import sciline
 import scipp as sc
 import scippnexus as snx
 
-from ess.reduce.nexus import load_component
 from ess.reduce.nexus.workflow import GenericNeXusWorkflow
 from ess.spectroscopy.types import (
     GoniometerAngles,
-    NeXusComponent,
-    NeXusComponentLocationSpec,
+    NeXusClass,
     NeXusFileSpec,
     RunType,
 )
 
 
-# TODO can override nx_class_for_source instead
 # See https://github.com/scipp/essreduce/issues/98
-def load_nexus_source_from_moderator(
-    location: NeXusComponentLocationSpec[snx.NXsource, RunType],
-) -> NeXusComponent[snx.NXsource, RunType]:
-    """Load a NeXus moderator as a source."""
-    return NeXusComponent[snx.NXsource, RunType](
-        load_component(location, nx_class=snx.NXmoderator)
-    )
+def moderator_class_for_source() -> NeXusClass[snx.NXsource]:
+    """Select NXmoderator as the source."""
+    return NeXusClass[snx.NXsource](snx.NXmoderator)
 
 
 def load_goniometer_angles(
@@ -50,6 +43,6 @@ def load_goniometer_angles(
 def LoadNeXusWorkflow() -> sciline.Pipeline:
     """Workflow for loading BIFROST NeXus files."""
     workflow = GenericNeXusWorkflow()
-    workflow.insert(load_nexus_source_from_moderator)
+    workflow.insert(moderator_class_for_source)
     workflow.insert(load_goniometer_angles)
     return workflow
