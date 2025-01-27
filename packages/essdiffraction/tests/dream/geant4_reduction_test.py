483fd257546ca2b33ad37b2197e0c1cbf6e363c8
Neil Vaytet <neil.vaytet@ess.eu>
2025-01-27 20:44:10 +0100

    start fixing tests

--- packages/essdiffraction/tests/dream/geant4_reduction_test.py
+++ packages/essdiffraction/tests/dream/geant4_reduction_test.py
@@ -8,6 +8,7 @@ import sciline
 import scipp as sc
 import scipp.testing
 import scippnexus as snx
+from scippneutron.chopper import DiskChopper
 from scippneutron.io.cif import Author
 
 import ess.dream.data  # noqa: F401
@@ -37,12 +38,14 @@ from ess.powder.types import (
     ReducedTofCIF,
     SampleRun,
     TofMask,
+    TofWorkflow,
     TwoThetaBins,
     TwoThetaMask,
     UncertaintyBroadcastMode,
     VanadiumRun,
     WavelengthMask,
 )
+from ess.reduce import time_of_flight
 from ess.reduce import workflow as reduce_workflow
 
 sample = sc.vector([0.0, 0.0, 0.0], unit='mm')
@@ -80,6 +83,98 @@ params = {
 }
 
 
+def dream_choppers():
+    psc1 = DiskChopper(
+        frequency=sc.scalar(14.0, unit="Hz"),
+        beam_position=sc.scalar(0.0, unit="deg"),
+        phase=sc.scalar(286 - 180, unit="deg"),
+        axle_position=sc.vector(value=[0, 0, 6.145], unit="m"),
+        slit_begin=sc.array(
+            dims=["cutout"],
+            values=[-1.23, 70.49, 84.765, 113.565, 170.29, 271.635, 286.035, 301.17],
+            unit="deg",
+        ),
+        slit_end=sc.array(
+            dims=["cutout"],
+            values=[1.23, 73.51, 88.035, 116.835, 175.31, 275.565, 289.965, 303.63],
+            unit="deg",
+        ),
+        slit_height=sc.scalar(10.0, unit="cm"),
+        radius=sc.scalar(30.0, unit="cm"),
+    )
+
+    psc2 = DiskChopper(
+        frequency=sc.scalar(-14.0, unit="Hz"),
+        beam_position=sc.scalar(0.0, unit="deg"),
+        phase=sc.scalar(-236, unit="deg"),
+        axle_position=sc.vector(value=[0, 0, 6.155], unit="m"),
+        slit_begin=sc.array(
+            dims=["cutout"],
+            values=[-1.23, 27.0, 55.8, 142.385, 156.765, 214.115, 257.23, 315.49],
+            unit="deg",
+        ),
+        slit_end=sc.array(
+            dims=["cutout"],
+            values=[1.23, 30.6, 59.4, 145.615, 160.035, 217.885, 261.17, 318.11],
+            unit="deg",
+        ),
+        slit_height=sc.scalar(10.0, unit="cm"),
+        radius=sc.scalar(30.0, unit="cm"),
+    )
+
+    oc = DiskChopper(
+        frequency=sc.scalar(14.0, unit="Hz"),
+        beam_position=sc.scalar(0.0, unit="deg"),
+        phase=sc.scalar(297 - 180 - 90, unit="deg"),
+        axle_position=sc.vector(value=[0, 0, 6.174], unit="m"),
+        slit_begin=sc.array(dims=["cutout"], values=[-27.6 * 0.5], unit="deg"),
+        slit_end=sc.array(dims=["cutout"], values=[27.6 * 0.5], unit="deg"),
+        slit_height=sc.scalar(10.0, unit="cm"),
+        radius=sc.scalar(30.0, unit="cm"),
+    )
+
+    bcc = DiskChopper(
+        frequency=sc.scalar(112.0, unit="Hz"),
+        beam_position=sc.scalar(0.0, unit="deg"),
+        phase=sc.scalar(215 - 180, unit="deg"),
+        # phase=sc.scalar(240 - 180, unit="deg"),
+        axle_position=sc.vector(value=[0, 0, 9.78], unit="m"),
+        slit_begin=sc.array(dims=["cutout"], values=[-36.875, 143.125], unit="deg"),
+        slit_end=sc.array(dims=["cutout"], values=[36.875, 216.875], unit="deg"),
+        slit_height=sc.scalar(10.0, unit="cm"),
+        radius=sc.scalar(30.0, unit="cm"),
+    )
+
+    t0 = DiskChopper(
+        frequency=sc.scalar(28.0, unit="Hz"),
+        beam_position=sc.scalar(0.0, unit="deg"),
+        phase=sc.scalar(280 - 180, unit="deg"),
+        axle_position=sc.vector(value=[0, 0, 13.05], unit="m"),
+        slit_begin=sc.array(dims=["cutout"], values=[-314.9 * 0.5], unit="deg"),
+        slit_end=sc.array(dims=["cutout"], values=[314.9 * 0.5], unit="deg"),
+        slit_height=sc.scalar(10.0, unit="cm"),
+        radius=sc.scalar(30.0, unit="cm"),
+    )
+
+    return {"psc1": psc1, "psc2": psc2, "oc": oc, "bcc": bcc, "t0": t0}
+
+
+@pytest.fixture(scope="module")
+def tof_workflow():
+    tof_wf = time_of_flight.tof_workflow(
+        simulated_neutrons=time_of_flight.simulate_beamline(
+            choppers=dream_choppers(),
+            neutrons=500_000,
+            seed=123,
+        ),
+        ltotal_range=(sc.scalar(70.0, unit='m'), sc.scalar(80.0, unit='m')),
+        error_threshold=1.0,
+    )
+
+    # Save expensive steps from being re-computed many times
+    return time_of_flight.cache_results(tof_wf)
+
+
 @pytest.fixture(params=["mantle", "endcap_backward", "endcap_forward"])
 def params_for_det(request):
     # Not available in simulated data
@@ -98,8 +193,9 @@ def make_workflow(params_for_det, *, run_norm):
     return wf
 
 
-def test_pipeline_can_compute_dspacing_result(workflow):
+def test_pipeline_can_compute_dspacing_result(workflow, tof_workflow):
     workflow = powder.with_pixel_mask_filenames(workflow, [])
+    workflow[TofWorkflow] = TofWorkflow(tof_workflow)
     result = workflow.compute(IofDspacing)
     assert result.sizes == {'dspacing': len(params[DspacingBins]) - 1}
     assert sc.identical(result.coords['dspacing'], params[DspacingBins])
