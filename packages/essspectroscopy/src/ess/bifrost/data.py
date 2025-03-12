7c9182cf16f7f7a68a14d44ea8d3c817e2f1f856
Jan-Lukas Wynen <jan-lukas.wynen@ess.eu>
2025-03-12 10:51:38 +0100

    Add tof table to data

--- packages/essspectroscopy/src/ess/bifrost/data.py
+++ packages/essspectroscopy/src/ess/bifrost/data.py
@@ -18,6 +18,7 @@ def _make_pooch():
         version=_version,
         registry={
             "BIFROST_20240914T053723.h5": "md5:0f2fa5c9a851f8e3a4fa61defaa3752e",
+            "BIFROST-simulation-tof-lookup-table.h5": "md5:9469b7b8c50463b1110bfbdffa2989d5",
         },
     )
 
@@ -38,3 +39,8 @@ def get_path(name: str) -> str:
 def simulated_elastic_incoherent_with_phonon() -> str:
     """Simulated data for elastic incoherent scattering including a phonon."""
     return get_path("BIFROST_20240914T053723.h5")
+
+
+def tof_lookup_table_simulation() -> str:
+    """Time-of-flight lookup table for the simulated BIFROST data."""
+    return get_path("BIFROST-simulation-tof-lookup-table.h5")
