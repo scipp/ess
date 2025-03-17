6f9d54ea547bd00240f74aa155f5d6173c6f0f3f
Jan-Lukas Wynen <jan-lukas.wynen@ess.eu>
2025-03-17 16:47:28 +0100

    Merge triplets in workflow

--- packages/essspectroscopy/src/ess/bifrost/detector.py
+++ packages/essspectroscopy/src/ess/bifrost/detector.py
@@ -1,4 +1,16 @@
-from ..spectroscopy.types import FinalEnergy
+import scipp as sc
+import scippnexus as snx
+
+from ess.spectroscopy.types import (
+    CalibratedDetector,
+    DetectorData,
+    DetectorPositionOffset,
+    FinalEnergy,
+    NeXusComponent,
+    NeXusTransformation,
+    RunType,
+)
+
 from .types import ArcNumber
 
 
@@ -24,11 +36,71 @@ def arc_number(final_energy: FinalEnergy) -> ArcNumber:
     :
         The arc index of the analyzer from which the neutron scattered
     """
-    import scipp as sc
-
     minimum = sc.scalar(2.7, unit='meV')
     step = sc.scalar(0.575, unit='meV')
     return sc.round((final_energy - minimum) / step).to(dtype='int')
 
 
-providers = (arc_number,)
+def get_calibrated_detector_bifrost(
+    detector: NeXusComponent[snx.NXdetector, RunType],
+    *,
+    transform: NeXusTransformation[snx.NXdetector, RunType],
+    offset: DetectorPositionOffset[RunType],
+) -> CalibratedDetector[RunType]:
+    """Extract the data array corresponding to a detector's signal field.
+
+    The returned data array includes coords and masks pertaining directly to the
+    signal values array, but not additional information about the detector.
+    The data array is reshaped to the logical detector shape.
+
+    This function is specific to BIFROST and differs from the generic
+    :func:`ess.reduce.nexus.workflow.get_calibrated_detector` in that it does not
+    fold the detectors into logical dimensions because the files already contain
+    the detectors in the correct shape.
+
+    Parameters
+    ----------
+    detector:
+        Loaded NeXus detector.
+    transform:
+        Transformation that determines the detector position.
+    offset:
+        Offset to add to the detector position.
+
+    Returns
+    -------
+    :
+        Detector data.
+    """
+
+    from ess.reduce.nexus.types import DetectorBankSizes
+    from ess.reduce.nexus.workflow import get_calibrated_detector
+
+    da = get_calibrated_detector(
+        detector=detector,
+        transform=transform,
+        offset=offset,
+        # The detectors are folded in the file, no need to do that here.
+        bank_sizes=DetectorBankSizes({}),
+    )
+    da = da.rename(dim_0='tube', dim_1='length')
+    return CalibratedDetector[RunType](da)
+
+
+def merge_triplets(*triplets: DetectorData[RunType]) -> DetectorData[RunType]:
+    """Merge BIFROST detector triplets into a single data array.
+
+    Parameters
+    ----------
+    triplets:
+        Data arrays to merge.
+
+    Returns
+    -------
+    :
+        Input data arrays stacked along the "triplet" dimension.
+    """
+    return DetectorData[RunType](sc.concat(triplets, dim="triplet"))
+
+
+providers = (arc_number, get_calibrated_detector_bifrost)
