852951fa591f62281f1fe06a795cf446da66c4ee
Jan-Lukas Wynen <jan-lukas.wynen@ess.eu>
2025-04-02 17:00:27 +0200

    Add stack_detectors

--- packages/essdiffraction/src/ess/powder/grouping.py
+++ packages/essdiffraction/src/ess/powder/grouping.py
@@ -1,6 +1,8 @@
 # SPDX-License-Identifier: BSD-3-Clause
-# Copyright (c) 2023 Scipp contributors (https://github.com/scipp)
+# Copyright (c) 2025 Scipp contributors (https://github.com/scipp)
 """Grouping and merging of pixels / voxels."""
+
+import scipp as sc
 
 from .types import (
     DspacingBins,
@@ -28,6 +30,26 @@ def focus_data_dspacing_and_two_theta(
     return FocussedDataDspacingTwoTheta[RunType](
         data.bin({twotheta_bins.dim: twotheta_bins, dspacing_bins.dim: dspacing_bins})
     )
+
+
+def stack_detectors(*detectors: sc.DataArray) -> sc.DataArray:
+    """Concatenate all inputs along a 'detector' dimension.
+
+    This function is intended to be used to reduce a workflow that
+    was mapped over detectors.
+
+    Parameters
+    ----------
+    detectors:
+        Data arrays for each detector bank.
+        Must all have the same shape.
+
+    Returns
+    -------
+    :
+        The inputs ``detectors`` concatenated along the 'detector' dimension.
+    """
+    return sc.concat(list(detectors), dim='detector')
 
 
 providers = (focus_data_dspacing, focus_data_dspacing_and_two_theta)
