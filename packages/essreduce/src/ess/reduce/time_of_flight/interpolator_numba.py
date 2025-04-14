e7ada002f3b7f9a73752bcd5b2fc935b90042941
Neil Vaytet <neil.vaytet@ess.eu>
2025-04-14 14:50:34 +0200

    remove code duplication and use single interpolate function

--- packages/essreduce/src/ess/reduce/time_of_flight/interpolator_numba.py
+++ packages/essreduce/src/ess/reduce/time_of_flight/interpolator_numba.py
@@ -11,84 +11,7 @@ def interpolate(
     values: np.ndarray,
     xp: np.ndarray,
     yp: np.ndarray,
-    fill_value: float,
-    out: np.ndarray,
-):
-    """
-    Linear interpolation of data on a 2D regular grid.
-
-    Parameters
-    ----------
-    x:
-        1D array of grid edges along the x-axis (size nx). They must be linspaced.
-    y:
-        1D array of grid edges along the y-axis (size ny). They must be linspaced.
-    values:
-        2D array of values on the grid. The shape must be (ny, nx).
-    xp:
-        1D array of x-coordinates where to interpolate (size N).
-    yp:
-        1D array of y-coordinates where to interpolate (size N).
-    fill_value:
-        Value to use for points outside of the grid.
-    out:
-        1D array where the interpolated values will be stored (size N).
-    """
-    if not (len(xp) == len(yp) == len(out)):
-        raise ValueError("Interpolator: all input arrays must have the same size.")
-
-    nx = len(x)
-    ny = len(y)
-    npoints = len(xp)
-    xmin = x[0]
-    xmax = x[nx - 1]
-    ymin = y[0]
-    ymax = y[ny - 1]
-    dx = x[1] - xmin
-    dy = y[1] - ymin
-
-    one_over_dx = 1.0 / dx
-    one_over_dy = 1.0 / dy
-    norm = one_over_dx * one_over_dy
-
-    for i in prange(npoints):
-        xx = xp[i]
-        yy = yp[i]
-
-        if (xx < xmin) or (xx > xmax) or (yy < ymin) or (yy > ymax):
-            out[i] = fill_value
-
-        else:
-            ix = nx - 2 if xx == xmax else int((xx - xmin) * one_over_dx)
-            iy = ny - 2 if yy == ymax else int((yy - ymin) * one_over_dy)
-
-            x1 = x[ix]
-            x2 = x[ix + 1]
-            y1 = y[iy]
-            y2 = y[iy + 1]
-
-            a11 = values[iy, ix]
-            a21 = values[iy, ix + 1]
-            a12 = values[iy + 1, ix]
-            a22 = values[iy + 1, ix + 1]
-
-            x2mxx = x2 - xx
-            xxmx1 = xx - x1
-
-            out[i] = (
-                (y2 - yy) * (x2mxx * a11 + xxmx1 * a21)
-                + (yy - y1) * (x2mxx * a12 + xxmx1 * a22)
-            ) * norm
-
-
-@njit(boundscheck=False, cache=True, fastmath=False, parallel=True)
-def interpolate_with_offset(
-    x: np.ndarray,
-    y: np.ndarray,
-    values: np.ndarray,
-    xp: np.ndarray,
-    yp: np.ndarray,
-    offset: np.ndarray,
+    offset: np.ndarray | None,
     fill_value: float,
     out: np.ndarray,
 ):
@@ -114,7 +37,7 @@ def interpolate_with_offset(
     out:
         1D array where the interpolated values will be stored (size N).
     """
-    if not (len(xp) == len(yp) == len(offset) == len(out)):
+    if not (len(xp) == len(yp) == len(out)):
         raise ValueError("Interpolator: all input arrays must have the same size.")
 
     nx = len(x)
@@ -132,7 +55,7 @@ def interpolate_with_offset(
     norm = one_over_dx * one_over_dy
 
     for i in prange(npoints):
-        xx = xp[i] + offset[i]
+        xx = xp[i] + (offset[i] if offset is not None else 0.0)
         yy = yp[i]
 
         if (xx < xmin) or (xx > xmax) or (yy < ymin) or (yy > ymax):
@@ -195,25 +118,14 @@ class Interpolator:
         pulse_offset: np.ndarray | None = None,
     ) -> np.ndarray:
         out = np.empty_like(times)
-        if pulse_offset is None:
-            interpolate(
-                x=self.time_edges,
-                y=self.distance_edges,
-                values=self.values,
-                xp=times,
-                yp=distances,
-                fill_value=self.fill_value,
-                out=out,
-            )
-        else:
-            interpolate_with_offset(
-                x=self.time_edges,
-                y=self.distance_edges,
-                values=self.values,
-                xp=times,
-                yp=distances,
-                offset=pulse_offset,
-                fill_value=self.fill_value,
-                out=out,
-            )
+        interpolate(
+            x=self.time_edges,
+            y=self.distance_edges,
+            values=self.values,
+            xp=times,
+            yp=distances,
+            offset=pulse_offset,
+            fill_value=self.fill_value,
+            out=out,
+        )
         return out
