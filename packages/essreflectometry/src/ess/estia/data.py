9c65377f0941f74619477b9ea722c3616bbf464c
Johannes Kasimir <johanneskasimir@gmail.com>
2025-04-29 16:37:50 +0200

    docs: add notebook and data for test case with simulated spin flip sample

--- packages/essreflectometry/src/ess/estia/data.py
+++ packages/essreflectometry/src/ess/estia/data.py
@@ -1,5 +1,7 @@
 # SPDX-License-Identifier: BSD-3-Clause
 # Copyright (c) 2025 Scipp contributors (https://github.com/scipp)
+from multiprocessing import Pool
+
 import scipp as sc
 
 from ..reflectometry.types import Filename, ReferenceRun, SampleRun
@@ -39,6 +41,27 @@ def _make_pooch():
             "examples/NiTiML.ref": "md5:9769b884dfa09d34b6ae449b640463a1",
             "examples/Si-SiO2.ref": "md5:436e2312e137b63bf31cc39064d28864",
             "examples/Si-Ni.ref": "md5:76fcfc655635086060387163c9175ab4",
+            # Spin flip example from McStas simulation.
+            # All runs have the same sample rotation angle but different samples.
+            # Each sample is measured using all four flipper setting.
+            "spin_flip_example/ground_truth_spin_down_reflectivity.h5": "md5:1b3f4c70be6e2d5bae35836c378a0762",  # noqa: E501
+            "spin_flip_example/ground_truth_spin_up_reflectivity.h5": "md5:77ded33407a5004587e475ede0312424",  # noqa: E501
+            "spin_flip_example/magnetic_supermirror_2_offoff.h5": "md5:4ab8b8a51d72bbafe577da79de18a5e5",  # noqa: E501
+            "spin_flip_example/magnetic_supermirror_2_offon.h5": "md5:d62d7291f4b45e6d7b64a0671c47f384",  # noqa: E501
+            "spin_flip_example/magnetic_supermirror_2_onoff.h5": "md5:fbc866ff8ee10ec8a081f7d5166e876a",  # noqa: E501
+            "spin_flip_example/magnetic_supermirror_2_onon.h5": "md5:6e4a5abdb9a1c92ca8c7eab76c6d3fb5",  # noqa: E501
+            "spin_flip_example/magnetic_supermirror_offoff.h5": "md5:aacee27dfe5459c48236f5a204e4e984",  # noqa: E501
+            "spin_flip_example/magnetic_supermirror_offon.h5": "md5:8241117da2b7ee7abbaac607d0be6ca7",  # noqa: E501
+            "spin_flip_example/magnetic_supermirror_onoff.h5": "md5:f43b512d754f310d1739f7bd2e462b49",  # noqa: E501
+            "spin_flip_example/magnetic_supermirror_onon.h5": "md5:03892f59bd7d5e4295d5432365182b75",  # noqa: E501
+            "spin_flip_example/spin_flip_sample_offoff.h5": "md5:d626ad0e38068121eafd869e82dd048f",  # noqa: E501
+            "spin_flip_example/spin_flip_sample_offon.h5": "md5:a3aa7605f54127e347dae5db18ca0f10",  # noqa: E501
+            "spin_flip_example/spin_flip_sample_onoff.h5": "md5:e23c8fdc9dbdffce41d9307e035141f3",  # noqa: E501
+            "spin_flip_example/spin_flip_sample_onon.h5": "md5:84b7cd5c3bc9a98e8d453f55efc12f8e",  # noqa: E501
+            "spin_flip_example/supermirror_offoff.h5": "md5:6e9864ff523cee64d615db1b6ef8e5b5",  # noqa: E501
+            "spin_flip_example/supermirror_offon.h5": "md5:9ce5de9b9c64b1fc637276ae8eebdf02",  # noqa: E501
+            "spin_flip_example/supermirror_onoff.h5": "md5:039828202328afc6cd66810423851cdc",  # noqa: E501
+            "spin_flip_example/supermirror_onon.h5": "md5:fffc4838b160b7f067592f01c067ec49",  # noqa: E501
         },
     )
 
@@ -102,8 +125,44 @@ def estia_mcstas_groundtruth(name):
     raise ValueError(f'"{name}" is not a valid sample name')
 
 
+def estia_mcstas_spin_flip_example(sample, flipper_setting):
+    return _pooch.fetch(f'spin_flip_example/{sample}_{flipper_setting}.h5')
+
+
+def estia_mcstas_spin_flip_example_groundtruth(up_or_down):
+    if up_or_down == 'down':
+        return _pooch.fetch('spin_flip_example/ground_truth_spin_down_reflectivity.h5')
+    if up_or_down == 'up':
+        return _pooch.fetch('spin_flip_example/ground_truth_spin_up_reflectivity.h5')
+    raise ValueError(f'Ground truth curve for spin state "{up_or_down}" does not exist')
+
+
+def _refresh_cache(args):
+    estia_mcstas_spin_flip_example(*args)
+
+
+def estia_mcstas_spin_flip_example_download_all_to_cache():
+    with Pool(20) as pool:
+        pool.map(
+            _refresh_cache,
+            [
+                (sample, setting)
+                for sample in (
+                    'supermirror',
+                    'magnetic_supermirror',
+                    'magnetic_supermirror_2',
+                    'spin_flip_sample',
+                )
+                for setting in ('offoff', 'offon', 'onoff', 'onon')
+            ],
+        )
+
+
 __all__ = [
     "estia_mcstas_example",
     "estia_mcstas_reference_run",
     "estia_mcstas_sample_run",
+    "estia_mcstas_spin_flip_example",
+    "estia_mcstas_spin_flip_example_download_all_to_cache",
+    "estia_mcstas_spin_flip_example_groundtruth",
 ]
