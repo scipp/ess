76afe0db158e005e771b0ce6a2bb44a67825e39a
YooSunyoung <luysunyoung9@gmail.com>
2025-04-30 15:47:17 +0200

    Argument is class.

--- packages/essreduce/tests/streaming_test.py
+++ packages/essreduce/tests/streaming_test.py
@@ -164,9 +164,9 @@ def test_min_accumulator() -> None:
         _ = accum.value
 
 
-@pytest.mark.parametrize("accum", [streaming.MinAccumulator, streaming.MaxAccumulator])
-def test_accumulator_non_scalar_raises(accum) -> None:
-    accum = accum()
+@pytest.mark.parametrize("Accum", [streaming.MinAccumulator, streaming.MaxAccumulator])
+def test_accumulator_non_scalar_raises(Accum) -> None:
+    accum = Accum()
     var = sc.array(dims=['x'], values=[1.0, 2.0, 3.0, 2.0, 1.0])
     accum.push(var)  # First push does not raise
     with pytest.raises(sc.DimensionError, match="Expected 0 dimensions"):
