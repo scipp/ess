15b29b8f3a1be02f62bd60dd68714c4496978046
Jan-Lukas Wynen <jan-lukas.wynen@ess.eu>
2025-05-09 16:06:53 +0200

    Add estia nexus workflow

--- packages/essreflectometry/src/ess/estia/__init__.py
+++ packages/essreflectometry/src/ess/estia/__init__.py
@@ -3,76 +3,31 @@
 import importlib.metadata
 
 from ..reflectometry import supermirror
-from ..reflectometry.types import (
-    BeamDivergenceLimits,
-    BeamSize,
-    DetectorSpatialResolution,
-    NeXusDetectorName,
-    RunType,
-    Position,
-    SampleRotationOffset,
-)
 from . import conversions, load, maskings, normalization, orso, resolution, workflow
 from .types import (
     AngularResolution,
     SampleSizeResolution,
     WavelengthResolution,
 )
-from .workflow import EstiaMcStasWorkflow
+from .workflow import EstiaMcStasWorkflow, EstiaWorkflow
 
 try:
     __version__ = importlib.metadata.version(__package__ or __name__)
 except importlib.metadata.PackageNotFoundError:
     __version__ = "0.0.0"
 
-
-providers = (
-    *reflectometry_providers,
-    *load.providers,
-    *conversions.providers,
-    *maskings.providers,
-    *workflow.providers,
-    *normalization.providers,
-    *orso.providers,
-)
-"""
-List of providers for setting up a Sciline pipeline.
-
-This provides a default Estia workflow including providers for loadings files.
-"""
-
-
-def default_parameters() -> dict:
-    return {
-        supermirror.MValue: sc.scalar(5, unit=sc.units.dimensionless),
-        supermirror.CriticalEdge: 0.022 * sc.Unit("1/angstrom"),
-        supermirror.Alpha: sc.scalar(0.25 / 0.088, unit=sc.units.angstrom),
-        BeamSize[RunType]: 2.0 * sc.units.mm,
-        DetectorSpatialResolution[RunType]: 0.0025 * sc.units.m,
-        Position[snx.NXsample, RunType]: sc.vector([0, 0, 0], unit="m"),
-        NeXusDetectorName: "detector",
-        BeamDivergenceLimits: (
-            sc.scalar(-0.75, unit='deg'),
-            sc.scalar(0.75, unit='deg'),
-        ),
-        SampleRotationOffset[RunType]: sc.scalar(0.0, unit='deg'),
-    }
-
-
-def EstiaWorkflow() -> sciline.Pipeline:
-    """
-    Workflow with default parameters for the Estia instrument.
-    """
-    return sciline.Pipeline(providers=providers, params=default_parameters())
-
-
 __all__ = [
     "AngularResolution",
     "EstiaMcStasWorkflow",
+    "EstiaWorkflow",
     "SampleSizeResolution",
     "WavelengthResolution",
     "conversions",
     "load",
+    "maskings",
+    "normalization",
+    "orso",
     "resolution",
     "supermirror",
+    "workflow",
 ]
--- packages/essreflectometry/src/ess/estia/workflow.py
+++ packages/essreflectometry/src/ess/estia/workflow.py
@@ -5,6 +5,8 @@ import sciline
 import scipp as sc
 import scippnexus as snx
 
+from ess.reduce import nexus
+
 from ..reflectometry import providers as reflectometry_providers
 from ..reflectometry import supermirror
 from ..reflectometry.types import (
@@ -12,22 +14,34 @@ from ..reflectometry.types import (
     BeamSize,
     DetectorSpatialResolution,
     NeXusDetectorName,
-    RunType,
     Position,
+    ReferenceRun,
+    RunType,
+    SampleRotationOffset,
+    SampleRun,
 )
-from . import conversions, load, maskings, normalization, orso, corrections
+from . import conversions, corrections, load, maskings, normalization, orso
 
-
-mcstas_providers = (
+_general_providers = (
     *reflectometry_providers,
-    *load.providers,
     *conversions.providers,
     *corrections.providers,
     *maskings.providers,
     *normalization.providers,
     *orso.providers,
 )
+
+mcstas_providers = (
+    *_general_providers,
+    *load.providers,
+)
 """List of providers for setting up a Sciline pipeline for McStas data.
+
+This provides a default Estia workflow including providers for loadings files.
+"""
+
+providers = (*_general_providers,)
+"""List of providers for setting up a Sciline pipeline data.
 
 This provides a default Estia workflow including providers for loadings files.
 """
@@ -40,15 +54,35 @@ def mcstas_default_parameters() -> dict:
         supermirror.Alpha: sc.scalar(0.25 / 0.088, unit=sc.units.angstrom),
         BeamSize[RunType]: 2.0 * sc.units.mm,
         DetectorSpatialResolution[RunType]: 0.0025 * sc.units.m,
-        Position[snx.NXsample, RunType]: sc.vector([0, 0, 0], unit="m"),
         NeXusDetectorName: "detector",
         BeamDivergenceLimits: (
             sc.scalar(-0.75, unit='deg'),
             sc.scalar(0.75, unit='deg'),
         ),
+        SampleRotationOffset[RunType]: sc.scalar(0.0, unit='deg'),
+    }
+
+
+def default_parameters() -> dict:
+    return {
+        NeXusDetectorName: "multiblade_detector",
     }
 
 
 def EstiaMcStasWorkflow() -> sciline.Pipeline:
     """Workflow for reduction of McStas data for the Estia instrument."""
-    return sciline.Pipeline(providers=mcstas_providers, params=mcstas_default_parameters())
+    return sciline.Pipeline(
+        providers=mcstas_providers, params=mcstas_default_parameters()
+    )
+
+
+def EstiaWorkflow() -> sciline.Pipeline:
+    """Workflow for reduction of data for the Estia instrument."""
+    workflow = nexus.GenericNeXusWorkflow(
+        run_types=[SampleRun, ReferenceRun], monitor_types=[]
+    )
+    for provider in providers:
+        workflow.insert(provider)
+    for name, param in default_parameters().items():
+        workflow[name] = param
+    return workflow
