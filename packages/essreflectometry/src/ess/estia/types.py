b68a80e0a409653fb6e0b84ec00a9296521753d6
Johannes Kasimir <johanneskasimir@gmail.com>
2025-06-02 16:17:46 +0200

    refactor: remove old polarization implementation

--- packages/essreflectometry/src/ess/estia/types.py
+++ packages/essreflectometry/src/ess/estia/types.py
@@ -1,31 +1,8 @@
 # Copyright (c) 2025 Scipp contributors (https://github.com/scipp)
-from typing import NewType, TypeVar
+from typing import NewType
 
-import sciline
 import scipp as sc
 
 WavelengthResolution = NewType("WavelengthResolution", sc.Variable)
 AngularResolution = NewType("AngularResolution", sc.Variable)
 SampleSizeResolution = NewType("SampleSizeResolution", sc.Variable)
-
-OffOff = NewType("OffOff", str)
-OffOn = NewType("OffOn", str)
-OnOff = NewType("OnOff", str)
-OnOn = NewType("OnOn", str)
-FlipperSetting = TypeVar("FlipperSetting", OffOff, OffOn, OnOff, OnOn)
-
-MagneticSample = NewType("MagneticSample", str)
-MagneticReference = NewType("MagneticReference", str)
-NonMagneticReference = NewType("NonMagneticReference", str)
-PolarizedRunType = TypeVar(
-    "PolarizedRunType", MagneticSample, MagneticReference, NonMagneticReference
-)
-
-
-class Intensity(
-    sciline.Scope[PolarizedRunType, FlipperSetting, sc.DataArray], sc.DataArray
-):
-    """Intensity distribution"""
-
-
-PolarizedReflectivityOverQ = NewType("PolarizedReflectivityOverQ", list[sc.DataArray])
