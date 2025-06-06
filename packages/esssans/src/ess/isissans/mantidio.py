c61936ffc4ee8ef099b4d34d7a1ff444af267ce2
Simon Heybrock <simon.heybrock@ess.eu>
2025-06-06 06:40:23 +0200

    Support ISIS histogram files

--- packages/esssans/src/ess/isissans/mantidio.py
+++ packages/esssans/src/ess/isissans/mantidio.py
@@ -4,6 +4,7 @@
 File loading functions for ISIS data using Mantid.
 """
 
+import threading
 from typing import NewType, NoReturn
 
 import sciline
@@ -99,12 +100,29 @@ def from_data_workspace(
 
 
 def load_run(filename: Filename[RunType], period: Period) -> DataWorkspace[RunType]:
-    loaded = _mantid_simpleapi.Load(
-        Filename=str(filename), LoadMonitors=True, StoreInADS=False
-    )
+    # Loading many small files with Mantid is, for some reason, very slow when using
+    # the default number of threads in the Dask threaded scheduler (1 thread worked
+    # best, 2 is a bit slower but still fast). We can either limit that thread count,
+    # or add a lock here, which is more specific.
+    with load_run.lock:
+        loaded = _mantid_simpleapi.Load(Filename=str(filename), StoreInADS=False)
+    try:
+        loaded = _mantid_simpleapi.Load(
+            Filename=str(filename), LoadMonitors=True, StoreInADS=False
+        )
+    except TypeError:
+        # Not loaded using LoadEventNexus, so LoadMonitor option is not available.
+        loaded = _mantid_simpleapi.Load(Filename=str(filename), StoreInADS=False)
     if isinstance(loaded, _mantid_api.Workspace):
         # A single workspace
         data_ws = loaded
+        if isinstance(data_ws, _mantid_api.WorkspaceGroup):
+            if period is None:
+                raise ValueError(
+                    f'Needs {Period} to be set to know what '
+                    'section of the event data to load'
+                )
+            data_ws = data_ws.getItem(period)
     else:
         # Separate data and monitor workspaces
         data_ws = loaded.OutputWorkspace
@@ -120,6 +138,8 @@ def load_run(filename: Filename[RunType], period: Period) -> DataWorkspace[RunTy
             data_ws.setMonitorWorkspace(loaded.MonitorWorkspace)
     return DataWorkspace[RunType](data_ws)
 
+
+load_run.lock = threading.Lock()
 
 providers = (
     from_data_workspace,
