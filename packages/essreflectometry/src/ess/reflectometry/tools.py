scipp/essreflectometry

fix: include resolution in stitched curve if present

src/ess/reflectometry/tools.py
@@ -261,7 +261,8 @@ def combine_curves(
     inv_v = 1.0 / v
     r_avg = np.nansum(r * inv_v, axis=0) / np.nansum(inv_v, axis=0)
     v_avg = 1 / np.nansum(inv_v, axis=0)
-    return sc.DataArray(
+
+    out = sc.DataArray(
         data=sc.array(
             dims='Q',
             values=r_avg,
@@ -270,3 +271,25 @@ def combine_curves(
         ),
         coords={'Q': qgrid},
     )
+    if any('Q_resolution' in c.coords for c in curves):
+        # This might need to be revisited. The question about how to combine curves
+        # with different Q-resolution is not completely resolved.
+        # However, in practice the difference in Q-resolution between different curves
+        # is small so it's not likely to make a big difference.
+        q_res = (
+            sc.DataArray(
+                data=c.coords.get(
+                    'Q_resolution', sc.scalar(float('nan')) * sc.values(c.data.copy())
+                ),
+                coords={'Q': c.coords['Q']},
+            )
+            for c in curves
+        )
+        qs = _interpolate_on_qgrid(q_res, qgrid).values
+        qs_avg = np.nansum(qs * inv_v, axis=0) / np.nansum(
+            ~np.isnan(qs) * inv_v, axis=0
+        )
+        out.coords['Q_resolution'] = sc.array(
+            dims='Q', values=qs_avg, unit=next(iter(curves)).coords['Q_resolution'].unit
+        )
+    return out

scipp/ess

packages/essreflectometry/src/ess/reflectometry/tools.py
@@ -229,7 +229,7 @@ def scale_reflectivity_curves_to_overlap(
 
 def combine_curves(
     curves: Sequence[sc.DataArray],
-    q_bin_edges: sc.Variable | None = None,
+    q_bin_edges: sc.Variable,
 ) -> sc.DataArray:
     '''Combines the given curves by interpolating them
     on a 1d grid defined by :code:`q_bin_edges` and averaging
@@ -268,7 +268,8 @@ def combine_curves(
     inv_v = 1.0 / v
     r_avg = np.nansum(r * inv_v, axis=0) / np.nansum(inv_v, axis=0)
     v_avg = 1 / np.nansum(inv_v, axis=0)
-    return sc.DataArray(
+
+    out = sc.DataArray(
         data=sc.array(
             dims='Q',
             values=r_avg,
@@ -277,6 +278,28 @@ def combine_curves(
         ),
         coords={'Q': q_bin_edges},
     )
+    if any('Q_resolution' in c.coords for c in curves):
+        # This might need to be revisited. The question about how to combine curves
+        # with different Q-resolution is not completely resolved.
+        # However, in practice the difference in Q-resolution between different curves
+        # is small so it's not likely to make a big difference.
+        q_res = (
+            sc.DataArray(
+                data=c.coords.get(
+                    'Q_resolution', sc.scalar(float('nan')) * sc.values(c.data.copy())
+                ),
+                coords={'Q': c.coords['Q']},
+            )
+            for c in curves
+        )
+        qs = _interpolate_on_qgrid(q_res, q_bin_edges).values
+        qs_avg = np.nansum(qs * inv_v, axis=0) / np.nansum(
+            ~np.isnan(qs) * inv_v, axis=0
+        )
+        out.coords['Q_resolution'] = sc.array(
+            dims='Q', values=qs_avg, unit=next(iter(curves)).coords['Q_resolution'].unit
+        )
+    return out
 
 
 def orso_datasets_from_measurements(