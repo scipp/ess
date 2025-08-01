scipp/essreflectometry

fix: use scipp nansum

src/ess/reflectometry/tools.py
@@ -254,19 +254,19 @@ def combine_curves(
     if len({c.coords['Q'].unit for c in curves}) != 1:
         raise ValueError('The Q-coordinates must have the same unit for each curve')
 
-    r = _interpolate_on_qgrid(map(sc.values, curves), qgrid).values
-    v = _interpolate_on_qgrid(map(sc.variances, curves), qgrid).values
+    r = _interpolate_on_qgrid(map(sc.values, curves), qgrid)
+    v = _interpolate_on_qgrid(map(sc.variances, curves), qgrid)
 
-    v[v == 0] = np.nan
+    v = sc.where(v == 0, sc.scalar(np.nan, unit=v.unit), v)
     inv_v = 1.0 / v
-    r_avg = np.nansum(r * inv_v, axis=0) / np.nansum(inv_v, axis=0)
-    v_avg = 1 / np.nansum(inv_v, axis=0)
+    r_avg = sc.nansum(r * inv_v, dim='curves') / sc.nansum(inv_v, dim='curves')
+    v_avg = 1 / sc.nansum(inv_v, dim='curves')
 
     out = sc.DataArray(
         data=sc.array(
             dims='Q',
-            values=r_avg,
-            variances=v_avg,
+            values=r_avg.values,
+            variances=v_avg.values,
             unit=next(iter(curves)).data.unit,
         ),
         coords={'Q': qgrid},
@@ -279,17 +279,14 @@ def combine_curves(
         q_res = (
             sc.DataArray(
                 data=c.coords.get(
-                    'Q_resolution', sc.scalar(float('nan')) * sc.values(c.data.copy())
+                    'Q_resolution', sc.full_like(c.coords['Q'], value=np.nan)
                 ),
                 coords={'Q': c.coords['Q']},
             )
             for c in curves
         )
-        qs = _interpolate_on_qgrid(q_res, qgrid).values
-        qs_avg = np.nansum(qs * inv_v, axis=0) / np.nansum(
-            ~np.isnan(qs) * inv_v, axis=0
-        )
-        out.coords['Q_resolution'] = sc.array(
-            dims='Q', values=qs_avg, unit=next(iter(curves)).coords['Q_resolution'].unit
+        qs = _interpolate_on_qgrid(q_res, qgrid)
+        out.coords['Q_resolution'] = sc.nansum(qs * inv_v, dim='curves') / sc.nansum(
+            sc.where(sc.isnan(qs), sc.scalar(0.0, unit=inv_v.unit), inv_v), dim='curves'
         )
     return out

scipp/ess

packages/essreflectometry/src/ess/reflectometry/tools.py
@@ -261,19 +261,19 @@ def combine_curves(
     if len({c.coords['Q'].unit for c in curves}) != 1:
         raise ValueError('The Q-coordinates must have the same unit for each curve')
 
-    r = _interpolate_on_qgrid(map(sc.values, curves), q_bin_edges).values
-    v = _interpolate_on_qgrid(map(sc.variances, curves), q_bin_edges).values
+    r = _interpolate_on_qgrid(map(sc.values, curves), q_bin_edges)
+    v = _interpolate_on_qgrid(map(sc.variances, curves), q_bin_edges)
 
-    v[v == 0] = np.nan
+    v = sc.where(v == 0, sc.scalar(np.nan, unit=v.unit), v)
     inv_v = 1.0 / v
-    r_avg = np.nansum(r * inv_v, axis=0) / np.nansum(inv_v, axis=0)
-    v_avg = 1 / np.nansum(inv_v, axis=0)
+    r_avg = sc.nansum(r * inv_v, dim='curves') / sc.nansum(inv_v, dim='curves')
+    v_avg = 1 / sc.nansum(inv_v, dim='curves')
 
     out = sc.DataArray(
         data=sc.array(
             dims='Q',
-            values=r_avg,
-            variances=v_avg,
+            values=r_avg.values,
+            variances=v_avg.values,
             unit=next(iter(curves)).data.unit,
         ),
         coords={'Q': q_bin_edges},
@@ -286,18 +286,15 @@ def combine_curves(
         q_res = (
             sc.DataArray(
                 data=c.coords.get(
-                    'Q_resolution', sc.scalar(float('nan')) * sc.values(c.data.copy())
+                    'Q_resolution', sc.full_like(c.coords['Q'], value=np.nan)
                 ),
                 coords={'Q': c.coords['Q']},
             )
             for c in curves
         )
-        qs = _interpolate_on_qgrid(q_res, q_bin_edges).values
-        qs_avg = np.nansum(qs * inv_v, axis=0) / np.nansum(
-            ~np.isnan(qs) * inv_v, axis=0
-        )
-        out.coords['Q_resolution'] = sc.array(
-            dims='Q', values=qs_avg, unit=next(iter(curves)).coords['Q_resolution'].unit
+        qs = _interpolate_on_qgrid(q_res, q_bin_edges)
+        out.coords['Q_resolution'] = sc.nansum(qs * inv_v, dim='curves') / sc.nansum(
+            sc.where(sc.isnan(qs), sc.scalar(0.0, unit=inv_v.unit), inv_v), dim='curves'
         )
     return out
 