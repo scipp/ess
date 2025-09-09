f7860915715dbf7cf5c5955b43c23658b9aa9c55
Johannes Kasimir <johanneskasimir@gmail.com>
2025-09-09 09:17:45 +0200

    docs

--- packages/essimaging/src/ess/imaging/tools/saturation_indicator.py
+++ packages/essimaging/src/ess/imaging/tools/saturation_indicator.py
@@ -8,6 +8,14 @@ def saturation_indicator(
 ) -> tuple[sc.DataArray, sc.Variable]:
     """
 
+    The intensity is supposed to be a signal that scales with the gain
+    of the image intensifier.
+    Near saturation that is not the case anymore.
+    The indicator tracks the second component of the singular value decomposition
+    of the intensity signal, at the gain when the amplitude of the second component
+    changes quickly that indicates saturation.
+    The indicator is scaled to have a maximum at the gain where saturation kicks in.
+
     Parameters
     -------------
     intensity:
@@ -23,8 +31,6 @@ def saturation_indicator(
         The saturation indicator value as a function of 'gain',
         and the maximum gain value acceptable according to the
         provided threshold.
-        Note that the minimum of the saturation indicator occurs
-        at the gain where saturation kicks in.
     """
     if intensity.dims != ('gain', 'wavelength'):
         raise ValueError(
@@ -35,10 +41,10 @@ def saturation_indicator(
     # The change in the amplitude of the second component indicates
     # a new component is present in the signal.
     indicator = np.linalg.svd(intensity.values, full_matrices=False)[0][:, 1]
-    # The sign of indicator is arbitrary, to make sure the extremum is a minimum
+    # The sign of indicator is arbitrary, to make sure the extremum is a maximum
     # multiply by the sign of the second derivative.
-    indicator *= np.sign(np.polyfit(gain.values, indicator, 2)[0])
+    indicator *= -np.sign(np.polyfit(gain.values, indicator, 2)[0])
 
     return sc.DataArray(
         sc.array(dims=['gain'], values=indicator), coords={'gain': gain}
-    ), threshold * gain['gain', np.argmin(indicator)]
+    ), threshold * gain['gain', np.argmax(indicator)]
