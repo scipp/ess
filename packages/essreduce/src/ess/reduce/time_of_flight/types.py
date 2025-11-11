bc58d9e9c88ad91e62eee2bdcabba626c73d1e35
Neil Vaytet <39047984+nvaytet@users.noreply.github.com>
2025-11-11 16:03:07 +0100

    Update src/ess/reduce/time_of_flight/types.py
    Co-authored-by: Simon Heybrock <12912489+SimonHeybrock@users.noreply.github.com>

--- packages/essreduce/src/ess/reduce/time_of_flight/types.py
+++ packages/essreduce/src/ess/reduce/time_of_flight/types.py
@@ -39,6 +39,7 @@ class TofDetector(sl.Scope[RunType, sc.DataArray], sc.DataArray):
 
 class ToaDetector(sl.Scope[RunType, sc.DataArray], sc.DataArray):
     """Detector data with time-of-arrival coordinate.
+
     When the pulse stride is 1 (i.e., no pulse skipping), the time-of-arrival is the
     same as the event_time_offset. When pulse skipping is used, the time-of-arrival is
     the event_time_offset + pulse_offset * pulse_period.
