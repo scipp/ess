cfaa884a8e2a8a7a09d6907982b875f5894dfc01
Johannes Kasimir <johanneskasimir@gmail.com>
2025-12-15 13:00:50 +0100

    test: result is unchanged

--- packages/esssans/tests/loki/iofq_test.py
+++ packages/esssans/tests/loki/iofq_test.py
@@ -283,6 +283,18 @@ def test_beam_center_from_center_of_mass_is_close_to_verified_result():
     assert sc.allclose(center, reference)
 
 
+def test_beam_center_from_center_of_mass_alternative_is_close_to_verified_result():
+    pipeline = make_workflow(no_masks=False)
+    pipeline = sans.with_pixel_mask_filenames(
+        pipeline, loki.data.loki_tutorial_mask_filenames()
+    )
+    center = sans.beam_center_finder.beam_center_from_center_of_mass_alternative(
+        pipeline
+    )
+    reference = sc.vector([-0.0248743, -0.0166965, 0], unit='m')
+    assert sc.allclose(center, reference)
+
+
 def test_phi_with_gravity():
     pipeline = make_workflow()
     pipeline[BeamCenter] = _compute_beam_center()
