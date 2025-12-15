e1a0aa7be0cea8e17ddbf88ef543fc7668b23098
Neil Vaytet <neil.vaytet@ess.eu>
2025-12-15 22:17:02 +0100

    refactor to inherit from VBox

--- packages/esssans/src/ess/loki/diagnostics.py
+++ packages/esssans/src/ess/loki/diagnostics.py
@@ -4,18 +4,10 @@
 
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, Any
-
 import ipywidgets as ipw
 import matplotlib.pyplot as plt
 import plopp as pp
 import scipp as sc
-
-if TYPE_CHECKING:
-    try:
-        from plopp.widgets import Box
-    except ModuleNotFoundError:
-        Box = object
 
 
 def _slice_or_sum(
@@ -125,19 +117,31 @@ class LokiBankViewer(ipw.VBox):
         )
 
 
-def instrument_view(
-    data: sc.DataArray | sc.DataGroup | dict,
-    dim: str | None = None,
-    pixel_size: float | sc.Variable | None = None,
-    autoscale: bool = False,
-    **kwargs: Any,
-) -> Box:
+def _to_data_group(data: sc.DataArray | sc.DataGroup | dict) -> sc.DataGroup:
+    if isinstance(data, sc.DataArray):
+        data = sc.DataGroup({data.name or "data": data})
+    elif isinstance(data, dict):
+        data = sc.DataGroup(data)
+    return data
+
+
+@pp.node
+def _pre_process(da: sc.DataArray, dim: str) -> sc.DataArray:
+    dims = list(da.dims)
+    if dim is not None:
+        dims.remove(dim)
+    out = da.flatten(dims=dims, to="pixel")
+    sel = sc.isfinite(out.coords["position"])
+    return out[sel]
+
+
+class InstrumentView(ipw.VBox):
     """
-    Three-dimensional visualization of the DREAM instrument.
+    Three-dimensional visualization of the Loki instrument.
     The instrument view is capable of slicing the input data with a slider widget along
     a dimension (e.g. ``tof``) by using the ``dim`` argument.
     It will also generate toggle buttons to hide/show the different modules that make up
-    the DREAM detectors.
+    the Loki detectors.
 
     Parameters
     ----------
@@ -157,43 +161,6 @@ def instrument_view(
         Additional arguments are forwarded to the scatter3d figure
         (see https://scipp.github.io/plopp/generated/plopp.scatter3d.html).
     """
-    from plopp.widgets import Box
-
-    if dim and isinstance(data, sc.DataArray) and dim in data.dims[:-1]:
-        data = data.transpose([d for d in data.dims if d != dim] + [dim])
-
-    if dim and isinstance(data, sc.DataGroup):
-        data = data.copy(deep=False)
-        for k, v in data.items():
-            if dim in v.dims[:-1]:
-                data[k] = v.transpose([d for d in v.dims if d != dim] + [dim])
-
-    view = InstrumentView(
-        data, dim=dim, pixel_size=pixel_size, autoscale=autoscale, **kwargs
-    )
-    return Box(view.children)
-
-
-def _to_data_group(data: sc.DataArray | sc.DataGroup | dict) -> sc.DataGroup:
-    if isinstance(data, sc.DataArray):
-        data = sc.DataGroup({data.name or "data": data})
-    elif isinstance(data, dict):
-        data = sc.DataGroup(data)
-    return data
-
-
-@pp.node
-def _pre_process(da: sc.DataArray, dim: str) -> sc.DataArray:
-    dims = list(da.dims)
-    if dim is not None:
-        dims.remove(dim)
-    out = da.flatten(dims=dims, to="pixel")
-    sel = sc.isfinite(out.coords["position"])
-    return out[sel]
-
-
-class InstrumentView:
-    """Instrument view for DREAM."""
 
     def __init__(
         self,
@@ -204,12 +171,21 @@ class InstrumentView:
     ):
         from plopp.widgets import SliceWidget, slice_dims
 
+        if dim and isinstance(data, sc.DataArray) and dim in data.dims[:-1]:
+            data = data.transpose([d for d in data.dims if d != dim] + [dim])
+
+        if dim and isinstance(data, sc.DataGroup):
+            data = data.copy(deep=False)
+            for k, v in data.items():
+                if dim in v.dims[:-1]:
+                    data[k] = v.transpose([d for d in v.dims if d != dim] + [dim])
+
         self.data = _to_data_group(data)
         self.pre_process_nodes = {
             key: _pre_process(da, dim) for key, da in self.data.items()
         }
 
-        self.children = []
+        self._widgets = []
 
         if dim is not None:
             self.slider = SliceWidget(next(iter(self.data.values())), dims=[dim])
@@ -220,7 +196,7 @@ class InstrumentView:
                 for key, n in self.pre_process_nodes.items()
             }
             to_scatter = self.slice_nodes
-            self.children.append(self.slider)
+            self._widgets.append(self.slider)
         else:
             self.slice_nodes = self.pre_process_nodes
             to_scatter = self.pre_process_nodes
@@ -233,10 +209,12 @@ class InstrumentView:
             **kwargs,
         )
 
-        self.children.insert(0, self.fig)
+        self._widgets.insert(0, self.fig)
 
         if len(self.data) > 1:
             self._add_module_control()
+
+        super().__init__(self._widgets)
 
     def _add_module_control(self):
         import ipywidgets as ipw
@@ -262,7 +240,7 @@ class InstrumentView:
         for key, b in self.buttons.items():
             b.key = key
             b.observe(self._check_visibility, names="value")
-        self.children.insert(0, self.modules_widget)
+        self._widgets.insert(0, self.modules_widget)
 
     def _check_visibility(self, _):
         active_nodes = [
