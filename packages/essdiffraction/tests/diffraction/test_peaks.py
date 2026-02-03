0c7ed43e04a5f8f47de081e2f658ffa23a105611
jokasimr <johanneskasimir@gmail.com>
2026-02-03 09:18:46 +0100

    feat: expose NC options in peak reader (#234)
    * feat: expose NC options in peak reader
    
    * Update src/ess/diffraction/peaks.py
    
    Co-authored-by: Jan-Lukas Wynen <jan-lukas.wynen@ess.eu>
    
    * docs: add example
    
    * fix
    
    * fix
    
    * fix
    
    ---------
    
    Co-authored-by: Jan-Lukas Wynen <jan-lukas.wynen@ess.eu>

--- packages/essdiffraction/tests/diffraction/test_peaks.py
+++ packages/essdiffraction/tests/diffraction/test_peaks.py
@@ -1,25 +1,33 @@
 import scipp as sc
 
-from ess.diffraction.peaks import dspacing_peak_positions_from_cif
+from ess.diffraction.peaks import dspacing_peaks_from_cif
 
 
 def test_retreived_peak_list_has_expected_units():
-    d = dspacing_peak_positions_from_cif(
+    d = dspacing_peaks_from_cif(
         'codid::9011998',
         sc.scalar(50, unit='barn'),
     )
-    assert d.unit == 'angstrom'
+    assert d.coords['dspacing'].unit == 'angstrom'
+    assert d.unit == 'barn'
 
 
 def test_intensity_threshold_is_taken_into_account():
-    d = dspacing_peak_positions_from_cif(
+    d = dspacing_peaks_from_cif(
         'codid::9011998',
         sc.scalar(50, unit='barn'),
     )
     assert len(d) > 0
 
-    d = dspacing_peak_positions_from_cif(
+    d = dspacing_peaks_from_cif(
         'codid::9011998',
         sc.scalar(50, unit='Mbarn'),
     )
     assert len(d) == 0
+
+
+def test_retreived_peak_list_with_temperature_kwarg():
+    d = dspacing_peaks_from_cif(
+        'codid::9011998', sc.scalar(50, unit='barn'), uiso_temperature=300
+    )
+    assert d.coords['uiso_temperature'] == 300
