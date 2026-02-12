730f22d89529e1c666df36932e038c287c90df22
Neil Vaytet <neil.vaytet@ess.eu>
2026-02-12 13:36:36 +0100

    update odin and tbl lookup tables and notebooks

--- packages/essimaging/src/ess/odin/data.py
+++ packages/essimaging/src/ess/odin/data.py
@@ -15,7 +15,7 @@ _registry = make_registry(
         "iron_simulation_sample_large.nxs": "md5:c162b6abeccb51984880d8d5002bae95",
         "iron_simulation_sample_small.nxs": "md5:dda6fb30aa88780c5a3d4cef6ea05278",
         "ODIN-tof-lookup-table.h5": "md5:e657021f4508f167b2a2eb550853b06b",
-        "ODIN-tof-lookup-table-5m-65m.h5": "md5:7b8b3afac20512935d9e6b44d740d06c",
+        "ODIN-tof-lookup-table-5m-65m.h5": "md5:c815eed6835a98d0b8d5252ffe250964",
     },
 )
 
@@ -60,26 +60,13 @@ def iron_simulation_ob_large() -> pathlib.Path:
     return _registry.get_path("iron_simulation_ob_large.nxs")
 
 
-def odin_tof_lookup_table(full_beamline: bool = False) -> pathlib.Path:
+def odin_tof_lookup_table() -> pathlib.Path:
     """
     Odin TOF lookup table.
-    This file is used to convert the time-of-flight to wavelength.
-    Use the ``full_beamline`` argument to get the lookup table for the full beamline,
-    which covers the range 5-65m.
-    The full range should be preferred, and the shorter range is kept for
-    retro-compatibility.
+    This file is used to convert the raw ``event_time_offset`` to time-of-flight.
 
-    These tables were computed using `Create a time-of-flight lookup table for ODIN
+    This table was computed using `Create a time-of-flight lookup table for ODIN
     <../../odin/odin-make-tof-lookup-table.rst>`_
     with ``NumberOfSimulatedNeutrons = 5_000_000``.
-
-    Parameters
-    ----------
-    full_beamline:
-        Whether to return the lookup table for the full beamline (5-65m) or for the
-        range 55-65m.
     """
-    if full_beamline:
-        return _registry.get_path("ODIN-tof-lookup-table-5m-65m.h5")
-    else:
-        return _registry.get_path("ODIN-tof-lookup-table.h5")
+    return _registry.get_path("ODIN-tof-lookup-table-5m-65m.h5")
