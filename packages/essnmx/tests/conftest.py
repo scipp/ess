2a289c16a96f589acce253c8fbc812aa873c0839
Sunyoung Yoo <17974113+YooSunYoung@users.noreply.github.com>
2026-03-18 17:47:02 +0100

    Move mcstas test helper to conftest. (#255)

--- packages/essnmx/tests/conftest.py
+++ packages/essnmx/tests/conftest.py
@@ -2,3 +2,4 @@
 # Copyright (c) 2024 Scipp contributors (https://github.com/scipp)
 # These fixtures cannot be found by pytest,
 # if they are not defined in `conftest.py` under `tests` directory.
+
