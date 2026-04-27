5253eac80e6fc49bcb212ec05be96ae005e3c7b4
Johannes Kasimir <johanneskasimir@gmail.com>
2026-04-27 10:14:21 +0200

    refactor: make helper with docstring

--- packages/essreflectometry/src/ess/estia/workflow.py
+++ packages/essreflectometry/src/ess/estia/workflow.py
@@ -113,6 +113,6 @@ def EstiaWorkflow() -> sciline.Pipeline:
         # In practice it should never be empty, and it cannot be reduced,
         # but this default makes it possible to at least load the data
         # for visualization.
-        lambda da: da.mean() if len(da) > 0 else sc.scalar(0.0, unit=da.unit)
+        corrections.assume_time_series_constant_with_zero_default_value_if_empty
     )
     return workflow
