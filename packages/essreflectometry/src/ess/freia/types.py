e172d004df1b02a83fe511a16104198cd87f4886
Johannes Kasimir <johanneskasimir@gmail.com>
2026-05-05 11:09:17 +0200

    feat: add monitor type for freia (WIP)

--- packages/essreflectometry/src/ess/freia/types.py
+++ packages/essreflectometry/src/ess/freia/types.py
@@ -1,8 +1,14 @@
 # Copyright (c) 2025 Scipp contributors (https://github.com/scipp)
-from typing import NewType
+from typing import NewType, TypeAlias
 
 import scipp as sc
+from ess.reduce.nexus.types import IncidentMonitor
+from ess.reduce.unwrap.types import WavelengthMonitor
+
+from ..reflectometry.types import RunType
 
 WavelengthResolution = NewType("WavelengthResolution", sc.Variable)
 AngularResolution = NewType("AngularResolution", sc.Variable)
 SampleSizeResolution = NewType("SampleSizeResolution", sc.Variable)
+
+WavelengthMonitor: TypeAlias = WavelengthMonitor[RunType, IncidentMonitor]
