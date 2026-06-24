7f4cdd203f983217e7390fc4cd697a7cd4261340
YooSunYoung <17974113+YooSunYoung@users.noreply.github.com>
2026-06-24 10:37:05 +0200

    Add docstring and script description.

--- packages/essnmx/tools/freeze-reduced.py
+++ packages/essnmx/tools/freeze-reduced.py
@@ -1,3 +1,20 @@
+"""Reduced file freezing script.
+
+Example:
+```
+pixi run --frozen python packages/essnmx/tools/freeze-reduced.py \
+        --freeze-version 26.6.0 \
+        --overwrite
+
+# This command will generate essnmx-reduced-26.6.0.hdf
+# and auxiliary outputs in the essnmx-reduced-26.6.0 directory.
+```
+
+Please note that the `freeze-version` is a hard-coded version
+and it does not automatically use specific version of essnmx.
+It is because it is meant to freeze a reduced file for a future release.
+"""
+
 import argparse
 
 import ess.nmx.data as nmxdat
@@ -9,7 +26,19 @@ from ess.nmx.configurations import (
 from ess.nmx.executables import reduction
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(
+        description="Generate a reduced file from "
+        "a small mcstas simulation result, injected into a nexus template. "
+        "The output file, essnmx-reduced-{version}.hdf, "
+        "should be published and be registered "
+        "in the `ess.nmx.data` module. "
+        "The registered file will be used for the executable-output-tests "
+        "that compare new reduction result to the frozen file. "
+        "When there is a breaking change, i.e. new version produces different result, "
+        "it should be documented in the developers guide, "
+        "under regression test dataset section. "
+        "The notebook file path: packages/essnmx/docs/developer/test-dataset.ipynb."
+    )
     parser.add_argument(
         "--freeze-version",
         help="Future version of essnmx that can reproduce the frozen file.",
