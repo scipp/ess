3e7e416e6bea98d3632086270a529d5e5c14d487
Johannes Kasimir <johanneskasimir@gmail.com>
2026-08-10 12:55:10 +0200

    test: add absolute tolerances to make tests less flaky

--- packages/essspectroscopy/tests/bifrost/workflow_test.py
+++ packages/essspectroscopy/tests/bifrost/workflow_test.py
@@ -114,12 +114,18 @@ def test_simulation_workflow_produces_the_same_data_as_before(
 
     assert set(expected.coords.keys()).issubset(set(energy_data.coords.keys()))
     for name in expected.coords.keys():
-        sc.testing.assert_allclose(energy_data.coords[name], expected.coords[name])
+        sc.testing.assert_allclose(
+            energy_data.coords[name],
+            expected.coords[name],
+            atol=sc.scalar(1e-15, unit=expected.coords[name].unit),
+        )
 
     assert energy_data.bins.coords.keys() == expected.bins.coords.keys()
     for name in energy_data.bins.coords.keys():
         sc.testing.assert_allclose(
-            energy_data.bins.coords[name], expected.bins.coords[name]
+            energy_data.bins.coords[name],
+            expected.bins.coords[name],
+            atol=sc.scalar(1e-15, unit=expected.bins.coords[name].unit),
         )
 
     sc.testing.assert_allclose(
